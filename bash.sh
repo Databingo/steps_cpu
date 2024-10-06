bfa4315cad53a5b5522ed5e9f21d6eb30aafe8d7
today072 <databingo@foxmail.com>
2024-10-06 21:13:45 +0800

    update

--- bash.sh
+++ bash.sh
@@ -1,7 +1,7 @@
 #iverilog -g2012 -o steps_cpu 2_load_program.v &&
-iverilog -g2012 -o steps_cpu 2_load_program.v  2tb.v &&
+iverilog -g2012 -o s2 2_load_program.v  2tb.v &&
 
-vvp steps_cpu
+vvp s2 
 
 #echo "finish" | vvp 
 
