9326a63b642e17b322ba97b16e47084aee7c28e5
today072 <databingo@foxmail.com>
2024-10-16 23:34:39 +0800

    update

--- 4_execute_instruction_2_64I.v
+++ 4_execute_instruction_2_64I.v
@@ -466,7 +466,6 @@ begin
 				           pc <= pc + 4; 
 	    	                           jp <=0;
 				         end 
-				  //+++++++++++++++++++++++++++++++++
 				  3'b101:begin 
                                            Lhu <= 1'b1; // set Lhu Flag  
 				           //load 16 bite unsign to 64 bits at imm(s1) to rd
@@ -475,7 +474,15 @@ begin
 				           pc <= pc + 4; 
 	    	                           jp <=0;
 				         end 
-			          3'b010: Lw  <= 1'b1; // set Lw  Flag 
+				  //+++++++++++++++++++++++++++++++++
+				  3'b010:begin 
+                                           Lw  <= 1'b1; // set Lw  Flag
+					   //load 32 bite sign extend to 64 bits at imm(s1) to rd
+				           rram[wire_rd] <= {{32{drom[rram[wire_rs1]+wire_imm][7]}}, drom[rram[wire_rs1]+wire_imm], drom[rram[wire_rs1]+wire_imm+1], drom[rram[wire_rs1]+wire_imm+2], drom[rram[wire_rs1]+wire_imm+3]}; 
+				           // prepare next instruction
+				           pc <= pc + 4; 
+	    	                           jp <=0;
+				         end 
 			          3'b110: Lwu <= 1'b1; // set Lwu Flag 
 			          3'b011: Ld  <= 1'b1; // set Ld  Flag 
 			        endcase
