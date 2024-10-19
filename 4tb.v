7a18ef9a076487b91f06a0c8fd79335d347c24b3
today072 <databingo@foxmail.com>
2024-10-19 21:38:48 +0800

    update

--- 4tb.v
+++ 4tb.v
@@ -302,9 +302,9 @@ always @(posedge clk) begin
 
 
    if (oimm !== 0 ) $write("oimm=%0b,",  oimm ,);
-   if (ox1 !== 0 ) $write("ox1=%0d,",  ox1 ,);
+   if (ox1 !== 0 ) $write("ox1=%0b,",  ox1 ,);
    //if (ox2 !== 0 ) $write("ox2=0x%0h,",  ox2 ,);
-   if (ox2 !== 0 ) $write("ox2=%0d,",  ox2 ,);
+   if (ox2 !== 0 ) $write("ox2=%0b,",  ox2 ,);
 
    if (oupimm  !== 0 ) $write("oupimm=%0b,",  oupimm ,);
 
