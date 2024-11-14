4ed99241677e9609005df4290336a1a63c2b21ba
today072 <databingo@foxmail.com>
2024-11-15 00:48:32 +0800

    update

--- sh.sh
+++ sh.sh
@@ -5,6 +5,22 @@
 #More space potentail
 #qemu-img resize xx.qcow2 +19G
 
+#sysctl -a|grep brand
+#machdep.cpu.brand: 0
+#machdep.cpu.brand_string: Intel(R) Core(TM) i5-4260U CPU @ 1.40GHz
+# Haswell x86_64-v3
+
+# FreeBSD
+# pkg install port-mgmt/fastest_pkg
+# mkdir -p /usr/local/etc/pkg/repos
+# echo 'FressBSD:{url:"https://mirrors.ustc.edu.cn/freebsd-pkg/${ABI}/latest",mirror_type:"NONE"}' > /usr/local/etc/pkg/repos/FreeBSD.conf
+# vi /boot/default/load.conf
+#kern.vty = sc  # color but [[
+#kern.vty = vt  # no-color
+#hw.vga.textmode = 2
+#kern.vt.fb.default_mode="1920x1080"
+
+
 ## start system
 #qemu-system-x86_64 \
 #-bios QEMU_EFI.fd -nographic \
@@ -19,6 +35,7 @@
 #-net nic -net user,hostfwd=tcp::2222-:22,hostfwd=tcp::7777-:7777,hostfwd=tcp::7778-:7778 \
 
 
+# for arch
 #qemu-system-x86_64 \
 #  -M pc \
 #  -m 3G \
@@ -29,17 +46,77 @@
 #  -device e1000,netdev=net0         # Network card compatible with x86
 
 
+# for fedora
+#qemu-system-x86_64 -d strace \
+#  -accel hvf \
+#  -M pc \
+#  -m 2G \
+#  -cpu host \
+#  -smp cores=2 \
+#  -drive file=./fedora-coreos-41.20241027.3.0-qemu.x86_64.qcow2,format=qcow2
+#  -device virtio-blk-device,drive=hd0   \
+#  -device virtio-net-device,netdev=net0 -nographic  -nographic
+#  -netdev user,id=net0,hostfwd=tcp::2222-:22,hostfwd=tcp::7777-:7777,hostfwd=tcp::7778-:7778 \
+
+
+# for Gentoo
+#qemu-img create -f qcow2 Gentoo-VM.img 15G 
+#qemu-system-x86_64 -d strace \
+#  -M pc \
+#  -accel hvf \
+#  -cpu host \
+#  -drive file=Gentoo-VM.img,format=qcow2,if=virtio \
+#  -netdev user,id=vmnic,hostname=Gentoo-VM,hostfwd=tcp::2222-:22,hostfwd=tcp::7777-:7777,hostfwd=tcp::7778-:7778 \
+#  -device virtio-net,netdev=vmnic \
+#  -device virtio-rng-pci \
+#  -m 2G \
+#  -smp cores=2 \
+#  -name "Gentoo VM" \
+#  -boot d \
+#  -cdrom  ~/Downloads/admincd-amd64-20241110T170333Z.iso
+#  #-monitor stdio \
+
+
+# for alpine
+#qemu-img create -f qcow2 Alpine.img 15G 
+#qemu-system-x86_64 -d strace \
+#  -M pc \
+#  -accel hvf \
+#  -cpu host \
+#  -drive file=Alpine.img,format=qcow2,if=virtio \
+#  -netdev user,id=vmnic,hostname=Gentoo-VM,hostfwd=tcp::2222-:22,hostfwd=tcp::7777-:7777,hostfwd=tcp::7778-:7778 \
+#  -device virtio-net,netdev=vmnic \
+#  -device virtio-rng-pci \
+#  -m 2G \
+#  -smp cores=2 \
+#  -name "Alpine" \
+#  -boot d \
+#  -cdrom  /Volumes/Ventoy/alpine-extended-3.20.3-x86_64.iso
+
+
+
+# for CentOS qcow2
+#qemu-system-x86_64 -d strace \
+#  -accel hvf \
+#  -M pc \
+#  -m 2G \
+#  -cpu host \
+#  -smp cores=2 \
+#  -drive file=/Users/chengang/Downloads/CentOS-Stream-GenericCloud-x86_64-9-latest.x86_64.qcow2,id=hd0,if=virtio \
+#  -netdev user,id=vmnic,hostfwd=tcp::2222-:22,hostfwd=tcp::7777-:7777,hostfwd=tcp::7778-:7778 \
+#  -device virtio-net,netdev=vmnic \
+
+
+# for freebsd
 qemu-system-x86_64 -d strace \
   -accel hvf \
   -M pc \
-  -m 2G \
+  -m 3G \
   -cpu host \
   -smp cores=2 \
-  -drive file=./fedora-coreos-41.20241027.3.0-qemu.x86_64.qcow2,format=qcow2
-  -device virtio-blk-device,drive=hd0   \
-  -device virtio-net-device,netdev=net0 -nographic  -nographic
-  -netdev user,id=net0,hostfwd=tcp::2222-:22,hostfwd=tcp::7777-:7777,hostfwd=tcp::7778-:7778 \
-# -netdev user,id=net0  \
- #-device e1000,netdev=net0         # Network card compatible with x86
-# -accel kmv \ for linux
- #-drive file=./Arch-Linux-x86_64-basic.qcow2,format=qcow2
+  -drive file=FreeBSD-14.1-RELEASE-amd64.qcow2,id=hd0,if=virtio \
+  -netdev user,id=vmnic,hostfwd=tcp::2222-:22,hostfwd=tcp::7777-:7777,hostfwd=tcp::7778-:7778 \
+  -device virtio-net,netdev=vmnic \
+  -vga std \
+  -display cocoa \
+  -full-screen \
