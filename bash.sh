6e6e14d9dd3d704f8374e3ee1cdc9b1f21d43d0c
today072 <databingo@foxmail.com>
2025-02-19 10:22:36 +0800

    update

--- bash.sh
+++ bash.sh
@@ -15,7 +15,7 @@
 #vvp s4 |less
 
 echo "" > binary_instructions.txt
-go run rvasm64I.go test.s
+go run rvasm64I.go test.s &&
 # s4.2
 iverilog -g2012 -o s4 4_execute_instruction_2_64I.v 4tb.v &&
 vvp s4 |less
