b462e9ad3c09022b791eafe80b78a96888a4dbe0
databingo <databingo@foxmail.com>
2025-03-29 10:19:54 +0800

    update

--- 4_execute_instruction_2_64I.v
+++ 4_execute_instruction_2_64I.v
@@ -1,6 +1,75 @@
 // 分步设计制作CPU 2024.10.04 解释权陈钢Email:databingo@foxmail.com
-`include "var.v"
+//`include "var.v"
 
+// 控制线
+reg Lui;
+reg Auipc; 
+reg Lb;
+reg Lbu;
+reg Lh; 
+reg Lhu;
+reg Lw;
+reg Lwu;
+reg Ld;
+
+reg Sb;
+reg Sh;
+reg Sw;
+reg Sd;
+
+reg Add;
+reg Sub;
+reg Sll;
+reg Slt;
+reg Sltu;
+reg Xor ;
+reg Srl;
+reg Sra;
+reg Or;
+reg And;
+
+reg Addi; 
+reg Slti;
+reg Sltiu;
+reg Ori; 
+reg Andi;
+reg Xori;
+reg Slli;
+reg Srli;
+reg Srai;
+
+reg Addiw;
+reg Slliw;
+reg Srliw;
+reg Sraiw;
+
+reg Addw;
+reg Subw;
+reg Sllw;
+reg Srlw;
+reg Sraw;
+
+reg Jal;   
+reg Jalr;
+
+reg Beq;
+reg Bne;
+reg Blt;
+reg Bge;
+reg Bltu;
+reg Bgeu;
+
+reg Fence;
+reg Fencei;    
+
+reg Ecall; 
+reg Ebreak;
+reg Csrrw;
+reg Csrrs;
+reg Csrrc;
+reg Csrrwi;
+reg Csrrsi;
+reg Csrrci;
 function [4:0] csr_index;
  input [11:0] csr_wire;
  begin
