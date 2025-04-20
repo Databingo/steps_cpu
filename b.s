7f68ce38e59af7e106f05b238b98e9acf2287dbb
databingo <databingo@foxmail.com>
2025-04-20 15:57:44 +0800

    update

--- b.s
+++ b.s
@@ -426,7 +426,209 @@ L_bgeu_v_6:
     li x30, 1
     li x11, 1
     li x11, 0
+##--------------------------------------------
+## Additional Branch Tests (Boundaries/Extremes) - RV64
+##--------------------------------------------
 
+# -- Signed Boundary Tests (BLT/BGE) --
+
+## TEST: BLT_MAXPOS_VS_MINNEG
+    # Purpose: Branch if MaxPos (0x7F...) < MinNeg (0x80...) -> Signed: False
+    li x5, 0x7FFFFFFFFFFFFFFF
+    li x6, 0x8000000000000000
+    blt x5, x6, L_blt_t_4      # Branch NOT Taken (Signed: Positive is not less than Negative)
+    li x31, 2                  # Path Not Taken Result = 2
+    j L_blt_v_7
+L_blt_t_4:
+    li x31, 1                  # Path Taken Result = 1
+L_blt_v_7:
+    li x30, 2                  # Expected Result = 2
+    li x11, 1
+    li x11, 0
+
+## TEST: BLT_MINNEG_VS_MAXPOS
+    # Purpose: Branch if MinNeg (0x80...) < MaxPos (0x7F...) -> Signed: True
+    li x5, 0x8000000000000000
+    li x6, 0x7FFFFFFFFFFFFFFF
+    blt x5, x6, L_blt_t_5      # Branch TAKEN (Signed: Negative is less than Positive)
+    li x31, 2
+    j L_blt_v_8
+L_blt_t_5:
+    li x31, 1
+L_blt_v_8:
+    li x30, 1
+    li x11, 1
+    li x11, 0
+
+## TEST: BGE_MAXPOS_VS_MINNEG
+    # Purpose: Branch if MaxPos (0x7F...) >= MinNeg (0x80...) -> Signed: True
+    li x5, 0x7FFFFFFFFFFFFFFF
+    li x6, 0x8000000000000000
+    bge x5, x6, L_bge_t_5      # Branch TAKEN (Signed: Positive is greater than Negative)
+    li x31, 2
+    j L_bge_v_7
+L_bge_t_5:
+    li x31, 1
+L_bge_v_7:
+    li x30, 1
+    li x11, 1
+    li x11, 0
+
+## TEST: BGE_MINNEG_VS_MAXPOS
+    # Purpose: Branch if MinNeg (0x80...) >= MaxPos (0x7F...) -> Signed: False
+    li x5, 0x8000000000000000
+    li x6, 0x7FFFFFFFFFFFFFFF
+    bge x5, x6, L_bge_nt_3     # Branch NOT Taken (Signed: Negative is not >= Positive)
+    li x31, 2
+    j L_bge_v_8
+L_bge_nt_3:
+    li x31, 1
+L_bge_v_8:
+    li x30, 2
+    li x11, 1
+    li x11, 0
+
+## TEST: BLT_NEG_ONE_VS_ZERO
+    # Purpose: Branch if -1 < 0 -> Signed: True
+    li x5, -1
+    li x6, 0
+    blt x5, x6, L_blt_t_6      # Branch TAKEN
+    li x31, 2
+    j L_blt_v_9
+L_blt_t_6:
+    li x31, 1
+L_blt_v_9:
+    li x30, 1
+    li x11, 1
+    li x11, 0
+
+## TEST: BGE_NEG_ONE_VS_ZERO
+    # Purpose: Branch if -1 >= 0 -> Signed: False
+    li x5, -1
+    li x6, 0
+    bge x5, x6, L_bge_nt_4     # Branch NOT Taken
+    li x31, 2
+    j L_bge_v_10
+L_bge_nt_4:
+    li x31, 1
+L_bge_v_10:
+    li x30, 2
+    li x11, 1
+    li x11, 0
+
+# -- Unsigned Boundary Tests (BLTU/BGEU) --
+
+## TEST: BLTU_MAXPOS_VS_MINNEG
+    # Purpose: Branch if MaxPos (0x7F...) < MinNeg (0x80...) -> Unsigned: True
+    li x5, 0x7FFFFFFFFFFFFFFF
+    li x6, 0x8000000000000000
+    bltu x5, x6, L_bltu_t_3    # Branch TAKEN (Unsigned: 0x7F... is less than 0x80...)
+    li x31, 2
+    j L_bltu_v_6
+L_bltu_t_3:
+    li x31, 1
+L_bltu_v_6:
+    li x30, 1
+    li x11, 1
+    li x11, 0
+
+## TEST: BLTU_MINNEG_VS_MAXPOS
+    # Purpose: Branch if MinNeg (0x80...) < MaxPos (0x7F...) -> Unsigned: False
+    li x5, 0x8000000000000000
+    li x6, 0x7FFFFFFFFFFFFFFF
+    bltu x5, x6, L_bltu_nt_4   # Branch NOT Taken (Unsigned: 0x80... is not less than 0x7F...)
+    li x31, 2
+    j L_bltu_v_7
+L_bltu_nt_4:
+    li x31, 1
+L_bltu_v_7:
+    li x30, 2
+    li x11, 1
+    li x11, 0
+
+## TEST: BGEU_MAXPOS_VS_MINNEG
+    # Purpose: Branch if MaxPos (0x7F...) >= MinNeg (0x80...) -> Unsigned: False
+    li x5, 0x7FFFFFFFFFFFFFFF
+    li x6, 0x8000000000000000
+    bgeu x5, x6, L_bgeu_nt_3   # Branch NOT Taken
+    li x31, 2
+    j L_bgeu_v_7
+L_bgeu_nt_3:
+    li x31, 1
+L_bgeu_v_7:
+    li x30, 2
+    li x11, 1
+    li x11, 0
+
+## TEST: BGEU_MINNEG_VS_MAXPOS
+    # Purpose: Branch if MinNeg (0x80...) >= MaxPos (0x7F...) -> Unsigned: True
+    li x5, 0x8000000000000000
+    li x6, 0x7FFFFFFFFFFFFFFF
+    bgeu x5, x6, L_bgeu_t_5    # Branch TAKEN
+    li x31, 2
+    j L_bgeu_v_8
+L_bgeu_t_5:
+    li x31, 1
+L_bgeu_v_8:
+    li x30, 1
+    li x11, 1
+    li x11, 0
+
+## TEST: BLTU_NEG_ONE_VS_ZERO
+    # Purpose: Branch if -1 (MaxUnsigned) < 0 -> Unsigned: False
+    li x5, -1                  # 0xFF...FF
+    li x6, 0
+    bltu x5, x6, L_bltu_nt_5   # Branch NOT Taken
+    li x31, 2
+    j L_bltu_v_8
+L_bltu_nt_5:
+    li x31, 1
+L_bltu_v_8:                 # Label typo fixed
+    li x30, 2
+    li x11, 1
+    li x11, 0
+
+## TEST: BGEU_NEG_ONE_VS_ZERO
+    # Purpose: Branch if -1 (MaxUnsigned) >= 0 -> Unsigned: True
+    li x5, -1                  # 0xFF...FF
+    li x6, 0
+    bgeu x5, x6, L_bgeu_t_6    # Branch TAKEN
+    li x31, 2
+    j L_bgeu_v_9
+L_bgeu_t_6:
+    li x31, 1
+L_bgeu_v_9:
+    li x30, 1
+    li x11, 1
+    li x11, 0
+
+## TEST: BLTU_MAXPOS_VS_NEG_ONE
+    # Purpose: Branch if MaxPos (0x7F...) < -1 (MaxUnsigned) -> Unsigned: True
+    li x5, 0x7FFFFFFFFFFFFFFF
+    li x6, -1                  # 0xFF...FF
+    bltu x5, x6, L_bltu_t_4    # Branch TAKEN
+    li x31, 2
+    j L_bltu_v_10
+L_bltu_t_4:
+    li x31, 1
+L_bltu_v_10:
+    li x30, 1
+    li x11, 1
+    li x11, 0
+
+## TEST: BGEU_MAXPOS_VS_NEG_ONE
+    # Purpose: Branch if MaxPos (0x7F...) >= -1 (MaxUnsigned) -> Unsigned: False
+    li x5, 0x7FFFFFFFFFFFFFFF
+    li x6, -1                  # 0xFF...FF
+    bgeu x5, x6, L_bgeu_nt_4   # Branch NOT Taken
+    li x31, 2
+    j L_bgeu_v_11
+L_bgeu_nt_4:
+    li x31, 1
+L_bgeu_v_11:
+    li x30, 2
+    li x11, 1
+    li x11, 0
 
 ##--------------------------------------------
 ## End of Branch Tests
