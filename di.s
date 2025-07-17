c62f077855e73d099e6de72153579a8a1217ac0f
youhao <youhaohao@haohaohao.com>
2025-07-17 17:19:00 +0800

    update

--- di.s
+++ di.s
@@ -1,25 +1,17 @@
-.section .data
-msg:    .asciz "Hello, RISC-V FreeBSD!\n"
+    .section .rodata
+msg:
+    .asciz "Hello from assembly main!\n"
 
-.section .text
-.global _start
-
-_start:
-    # a0 = file descriptor (1 = stdout)
-    li      a0, 1
-
-    # a1 = pointer to string
-    la      a1, msg
-
-    # a2 = length of string
-    li      a2, 22  # length of "Hello, RISC-V FreeBSD!\n"
-
-    # a7 = syscall number (write = 4 for FreeBSD)
-    li      a7, 4
-
+    .text
+    .globl main
+main:
+    # write(1, msg, 24)
+    li      a0, 1          # fd = 1 (stdout)
+    la      a1, msg        # buf = msg
+    li      a2, 24         # count = 24
+    li      a7, 4          # syscall: write
     ecall
 
-    # exit(0)
-    li      a0, 0      # exit code
-    li      a7, 93     # syscall number for exit
-    ecall
+    # return 0
+    li      a0, 0
+    ret
