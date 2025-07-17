6b880539218257971bb5248e3f660c06a8fe2879
youhao <youhaohao@haohaohao.com>
2025-07-17 12:34:03 +0800

    update

--- di.s
+++ di.s
@@ -11,15 +11,24 @@ msg:
 # Directive: Switch to text section for code
 .section .text
 main:
-    # Prepare for write(1, message_addr, length) syscall (Linux RV64)
+    # Prepare for write(1, message_addr, length) syscall (FreeBSD RV64)
     li      a7, 4          # write syscall number = 4
     li      a0, 1           # fd = 1 (stdout)
     la      a1, msg
-    li      a2, 13          # length = 13 (bytes in "H\n")
+    li      a2, 13          # length = 13 (bytes in "Hello RISC-V\n")
     ecall                   # Make the system call
-
+    
+    # Check if write failed (return value < 0)
+    bltz    a0, write_error
+    
     # Prepare for exit(0) syscall
     li      a7, 1          # exit syscall number = 1
     li      a0, 0           # exit code 0
+    ecall                   # Make the system call
+
+write_error:
+    # If write failed, exit with error code 1
+    li      a7, 1          # exit syscall number = 1
+    li      a0, 1           # exit code 1 (error)
     ecall                   # Make the system call
     ret
