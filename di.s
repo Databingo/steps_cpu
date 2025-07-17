7e1852af9e27a9f838b08a02fcad7bd44928fdb2
youhao <youhaohao@haohaohao.com>
2025-07-17 22:28:40 +0800

    update

--- di.s
+++ di.s
@@ -1,17 +1,20 @@
 .globl _start
 
-    .section .data
+.section .data
 msg:
-    .string "Hello, syscall!\n"
+    .string "Hello, minimal ELF!\n"
 
-    .section .text
+.section .text
 _start:
+    # write(1, msg, 20)
     li      a0, 1          # fd = 1 (stdout)
     la      a1, msg        # buf = msg
-    li      a2, 15         # count = 15 (length of string)
+    li      a2, 20         # count = 20 (length of string)
     li      a7, 4          # syscall: write (FreeBSD RISC-V)
     ecall
 
-    li      a0, 0          # exit code
+    # exit(0)
+    li      a0, 0
     li      a7, 93         # syscall: exit
     ecall
+
