6c079c4278e274591a8a7d72e366033ab275f07b
youhao <youhaohao@haohaohao.com>
2025-07-17 09:23:39 +0800

    update

--- di.s
+++ di.s
@@ -11,32 +11,11 @@ msg:
 # Directive: Switch to text section for code
 .section .text
 main:
-#    # Load the address of msg into t0 (a0 for syscall)
-#    # 'la' is a pseudo-instruction handled by assembler/linker
-#    la      a1, msg
-#    nop
-#    call _start
-#    tail _start
-#    j _start
-#    jr x9
-#    jal _start
-#    jalr x9
-#    mv x1, x2
-#    not x1, x3
-#    lb x1, _start
-#    lh x1, _start
-#    lw x1, _start
-#    ld x1, _start
-#    sb x1, _start, x2
-#    sh x1, _start, x2
-#    sw x1, _start, x2
-#    sd x1, _start, x2
-
     # Prepare for write(1, message_addr, length) syscall (Linux RV64)
     li      a7, 4          # write syscall number = 4
     li      a0, 1           # fd = 1 (stdout)
     la      a1, msg
-    li      a2, 14          # length = 13 (bytes in "H\n")
+    li      a2, 13          # length = 13 (bytes in "H\n")
     ecall                   # Make the system call
 
     # Prepare for exit(0) syscall
@@ -44,14 +23,3 @@ main:
     li      a0, 0           # exit code 0
     ecall                   # Make the system call
     ret
-
-
-#auipc a1, 0
-#addi  a1, a1, 0
-#li    a0, 1
-#li    a2, 1
-#li    a7, 4
-#ecall
-#li    a0, 0
-#li    a7, 1 
-#ecall
