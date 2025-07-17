e8fcd04263e68745d1c99e26d84627ff8f0cea18
youhao <youhaohao@haohaohao.com>
2025-07-17 23:18:02 +0800

    update

--- di.s
+++ di.s
@@ -1,20 +1,13 @@
-.globl _start
+.global main
+.global msg
 
 .section .data
 msg:
-    .string "Hello, minimal ELF!\n"
+    .string "Hello from assembly main!\n"
 
-.section .text
-_start:
-    # write(1, msg, 20)
-    li      a0, 1          # fd = 1 (stdout)
-    la      a1, msg        # buf = msg
-    li      a2, 20         # count = 20 (length of string)
-    li      a7, 4          # syscall: write (FreeBSD RISC-V)
-    ecall
-
-    # exit(0)
-    li      a0, 0
-    li      a7, 93         # syscall: exit
-    ecall
-
+.section   .text
+main:
+    la      a0, msg      # argument: pointer to string
+    call    puts         # call libc's puts
+    li      a0, 0        # return 0
+    ret
