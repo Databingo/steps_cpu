4b9e6ac8a4d0552b08a2df36515bd8248c79bbbc
youhao <youhaohao@haohaohao.com>
2025-07-21 08:44:06 +0800

    update

--- llama2.c/buildq.sh
+++ llama2.c/buildq.sh
@@ -3,12 +3,13 @@
 set -e
 XPACK_TOOLCHAIN_BIN="/usr/local/projects/bin/xpack-riscv-none-elf-gcc-14.2.0-3/bin"
 TARGET_PREFIX="${XPACK_TOOLCHAIN_BIN}/riscv-none-elf-"
-CFLAGS="-march=rv64gc -mabi=lp64d -g -O3 -ffreestanding -nostdlib -mcmodel=medany"
+CFLAGS="-march=rv64gcv -mabi=lp64d -g -O3 -ffreestanding -nostdlib -mcmodel=medany"
 LDFLAGS="-T linker.ld -nostdlib -lm"
 
 echo "--- Compiling QUANTIZED C and Assembly Code ---"
 "${TARGET_PREFIX}gcc" $CFLAGS -c start.S -o start.o
-"${TARGET_PREFIX}gcc" $CFLAGS -c runq_baremetal.c -o runq.o
+#"${TARGET_PREFIX}gcc" $CFLAGS -c runq_baremetal.c -o runq.o
+"${TARGET_PREFIX}gcc" $CFLAGS -c rq.c -o runq.o
 
 echo "--- Linking Executable ---"
 "${TARGET_PREFIX}gcc" $CFLAGS -o kernel_q80.elf start.o runq.o $LDFLAGS
--- llama2.c/qemuq.sh
+++ llama2.c/qemuq.sh
@@ -2,7 +2,7 @@
 qemu-system-riscv64 \
     -machine virt \
     -nographic \
-    -m 128M \
+    -m 1024M \
     -bios none \
     -icount shift=7,align=off,sleep=off \
     -kernel kernel_q80.bin
