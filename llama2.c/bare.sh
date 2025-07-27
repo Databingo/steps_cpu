6924e2631d79690e37c7b4a492d097a3464433ff
youhao <youhaohao@haohaohao.com>
2025-07-27 16:15:22 +0800

    update

--- llama2.c/bare.sh
+++ llama2.c/bare.sh
@@ -1,6 +1,7 @@
 #python3 export.py stories15M_q80.bin --checkpoint stories15M.pt --version 2
 #xxd -i stories15M_q80.bin > model_q80.h 
 #qemu-system-riscv64 -machine help
+#smaller llama2 LLM:SparseLlama-2-7b-pruned_50.2of4 
 #
 bash buildq.sh
 bash qemuq.sh
