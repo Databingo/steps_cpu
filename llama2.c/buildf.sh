84aa84a5c46880ba74c08a82f34854ce4356cf57
youhao <youhaohao@haohaohao.com>
2025-07-29 09:03:14 +0800

    update

--- llama2.c/buildf.sh
+++ llama2.c/buildf.sh
@@ -4,6 +4,8 @@ set -e
 XPACK_TOOLCHAIN_BIN="/usr/local/projects/bin/xpack-riscv-none-elf-gcc-14.2.0-3/bin"
 TARGET_PREFIX="${XPACK_TOOLCHAIN_BIN}/riscv-none-elf-"
 #CFLAGS="-march=rv64gcv -mabi=lp64d -g -O3 -ffreestanding -nostdlib -mcmodel=medany"
+#CFLAGS="-march=rv64imafd -mno-relax -mabi=lp64d -g -O3 -ffreestanding -nostdlib -mcmodel=medany"
+#LDFLAGS="-T linker.ld -nostdlib -lm -mno-relax"
 CFLAGS="-march=rv64gc -mabi=lp64d -g -O3 -ffreestanding -nostdlib -mcmodel=medany"
 LDFLAGS="-T linker.ld -nostdlib -lm"
 
