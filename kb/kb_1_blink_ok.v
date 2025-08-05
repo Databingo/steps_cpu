1157d018245846e1223eec30fcbf5e605f7c1a75
youhao <youhaohao@haohaohao.com>
2025-08-05 11:01:58 +0800

    update

---
+++ kb/kb_1_blink_ok.v
@@ -0,0 +1,31 @@
+module kb (in,out,clk);
+
+input in,clk;
+output reg out;
+reg [22:0] count;
+	always @(posedge(clk))
+		begin
+			if(in==1)
+				begin
+					if (count == 0)
+						begin 
+							out <= ~out;
+						end
+						count <= count + 1;
+				end 
+				else 
+					begin 
+						out <= 0;
+						count <= 1;
+					end
+				end	
+endmodule
+
+// -----Blink ok-----
+// Device: EP2C20F484C7
+// Handbook:
+// Cyclone II FPGA Starter Board
+// Pin map:
+// in : SW0-PIN_R20
+// out: LEDR0-PIN_L22
+// clk: CLK-PIN_L1 
