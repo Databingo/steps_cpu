2639bd8a3fe0d90a0cdb203f5688ad70100c386a
youhao <youhaohao@haohaohao.com>
2025-08-10 16:02:43 +0800

    update

--- bash.sh
+++ bash.sh
@@ -23,12 +23,12 @@ echo "" > binary_instructions.txt
 #go run rvas.go lw_lwu.s &&
 #go run rvas.go ld.s &&
 #go run rvas.go s.s &&
-go run rvas.go add_sub.s &&
+#go run rvas.go add_sub.s &&
 #go run rvas.go slt_u.s &&
 #go run rvas.go slti_u.s &&
 #go run rvas.go gate.s &&
 #go run rvas.go gate_i.s &&
-#go run rvas.go sh.s &&
+go run rvas.go sh.s &&
 #go run rvas.go sh_i.s &&
 #go run rvas.go b.s &&
 # go run rvas.go auipc.s &&
