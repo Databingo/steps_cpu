54ff2bb895b1c1e82aec87d89da73d94096be039
youhao <youhaohao@haohaohao.com>
2025-08-12 23:06:54 +0800

    update

--- cpu_on_board/cpu_on_board.v
+++ cpu_on_board/cpu_on_board.v
@@ -21,15 +21,4 @@ module cpu_on_board (
             end
         end
     end
-    
-    // --- LED Output Logic ---
-    // This is now a separate, simple piece of logic.
-    // The LEDs will be ON for the first half of the count cycle,
-    // and OFF for the second half.
-    // We use a simple comparison with the halfway point of the counter.
-    //assign LEDG = (counter < 25000000) ? 8'hFF : 8'h00;
-    
-    // Whoops, that's not quite right. Let's make it even simpler.
-    // We will use a single register to hold the blinking state.
-
 endmodule
