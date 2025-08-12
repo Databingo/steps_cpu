a9d1991d739b59176cfa7691511b14017fa034d0
youhao <youhaohao@haohaohao.com>
2025-08-12 17:03:55 +0800

    update

--- cpu_on_board/mif.py
+++ cpu_on_board/mif.py
@@ -1,10 +1,11 @@
 with open('../binary_instructions.txt', 'r') as f, open ('mem.mif', 'w') as out:
-    out.write("DEPTH = 19999:\nWIDTH = 8;\nADDRESS_RADIX = DEC;\nDATA_RADIX = BIN;\nCONTENT BEGIN\n")
+    #out.write("DEPTH = 19999:\nWIDTH = 8;\nADDRESS_RADIX = DEC;\nDATA_RADIX = BIN;\nCONTENT BEGIN\n")
     addr = 0
     for line in f:
         byte = line.split("//")[0]
         byte = byte.strip()
         if byte:
-            out.write(f"{addr} : {byte};\n")
+            #out.write(f"{addr} : {byte};\n")
+            out.write(f"{byte}\n")
             addr += 1
-    out.write("END;\n")
+    #out.write("END;\n")
