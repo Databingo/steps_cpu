51d859e158f779aba21b8c1adb688a4a7afb2ff8
youhao <youhaohao@haohaohao.com>
2025-08-15 15:28:41 +0800

    update

--- cpu_on_board/cpu_on_board.v
+++ cpu_on_board/cpu_on_board.v
@@ -10,7 +10,7 @@ module cpu_on_board (
 );
 
   
-    (* ram_style = "block" *) reg [31:0] mem [0:2999]; // Unified Memory
+    (* ram_style = "block" *) reg [7:0] mem [0:2999]; // Unified Memory
     //initial $readmemh("mem.mif", mem);
     initial $readmemb("mem.mif", mem);
 
