c8832b70f2d651ffcc22224da932562f2460eb2b
youhao <youhaohao@haohaohao.com>
2025-08-24 11:44:49 +0800

    update

---
+++ cpu_on_board/riscv64.v
@@ -0,0 +1,44 @@
+module riscv64(
+    input wire clk, 
+    input wire reset,     // Active-low reset button
+    input wire [31:0] instruction,
+    output reg [31:0] pc,
+    output reg [31:0] ir,
+    output reg [63:0] re [0:31],
+    output wire  heartbeat
+);
+
+    
+    // --- Immediate decoders (Unchanged) --- 
+    wire signed [63:0] w_imm_u = {{32{ir[31]}}, ir[31:12], 12'b0};
+    wire [4:0] w_rd  = ir[11:7];
+
+    // IF ir (Unchanged)
+    always @(posedge clk or negedge reset) begin
+        if (!reset) begin 
+            heartbeat <= 1'b0; 
+            ir <= 32'h00000000; 
+        end else begin
+            heartbeat <= ~heartbeat; // heartbeat
+            //ir <= ir_ld;
+            ir <= instruction;
+        end
+    end
+
+    // EXE pc (Unchanged, CPU runs normally)
+    always @(posedge clk or negedge reset) begin
+        if (!reset) begin 
+            pc <= 0;
+        end else begin
+            pc <= pc + 4;
+            re[31] <= 1'b0; // This was in your original code
+            
+	    //data <= 32'h48;
+            casez(ir) 
+		32'b???????_?????_?????_???_?????_0110111:  re[w_rd] <= w_imm_u; // Lui
+		//32'b???????_?????_?????_???_?????_0110111:  begin re[w_rd] <= w_imm_u; data <= 32'h41; end
+            endcase
+        end
+    end
+
+endmodule
