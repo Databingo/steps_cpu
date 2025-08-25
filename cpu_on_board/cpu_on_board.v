224d219e4d2b7b7a9fc23f47b54fefb19db37907
youhao <youhaohao@haohaohao.com>
2025-08-25 09:06:54 +0800

    update

--- cpu_on_board/cpu_on_board.v
+++ cpu_on_board/cpu_on_board.v
@@ -39,13 +39,6 @@ module cpu_on_board (
         .bus_read_data(bus_read_data)
     );
      
-    // -- Bus --
-    wire [63:0] bus_address;
-    wire [63:0] bus_write_data;
-    wire        bus_write_enable;
-    wire        bus_read_enable;
-    wire [63:0] bus_read_data;
-
     // -- Clock --
     wire clock_1hz;
     clock_slower clock_ins(
@@ -54,23 +47,23 @@ module cpu_on_board (
         .reset_n(KEY0)
     );
 
-   // -- Keyboard -- 
-   reg [31:0] data;
-   wire key_pressed;
-   wire key_released;
-   reg key_pressed_delay;
-   always @(posedge CLOCK_50) begin key_pressed_delay <= key_pressed; end
-   ps2_decoder ps2_decoder_inst (
-       .clk(CLOCK_50),
-       .ps2_clk_async(PS2_CLK),
-       .ps2_data_async(PS2_DAT),
-       //.scan_code(data[7:0])
-       .ascii_code(data[7:0]),
-       .key_pressed(key_pressed),
-       .key_released(key_released)
-   );
+    // -- Keyboard -- 
+    reg [31:0] data;
+    wire key_pressed;
+    wire key_released;
+    reg key_pressed_delay;
+    always @(posedge CLOCK_50) begin key_pressed_delay <= key_pressed; end
+    ps2_decoder ps2_decoder_inst (
+        .clk(CLOCK_50),
+        .ps2_clk_async(PS2_CLK),
+        .ps2_data_async(PS2_DAT),
+        //.scan_code(data[7:0])
+        .ascii_code(data[7:0]),
+        .key_pressed(key_pressed),
+        .key_released(key_released)
+     );
 
-   // -- Monitor --
+    // -- Monitor --
     wire [0:0]  avalon_address;
     wire        avalon_write;
     wire [31:0] avalon_writedata;
@@ -85,15 +78,20 @@ module cpu_on_board (
         .jtag_uart_0_avalon_jtag_slave_read_n    (1'b1)
     );
 
-   // -- Bus --
-   wire key_pressed_edge = key_pressed && !key_pressed_delay;
-   assign avalon_write     = key_pressed_edge; // Force the write signal high every cycle
-   assign avalon_address   = 1'b0;            // Always write to the data register (address 0)
-   assign avalon_writedata = {24'b0, data};    
+    // -- Bus --
+    wire key_pressed_edge = key_pressed && !key_pressed_delay;
+    assign avalon_write     = key_pressed_edge; // Force the write signal high every cycle
+    assign avalon_address   = 1'b0;            // Always write to the data register (address 0)
+    assign avalon_writedata = {24'b0, data};    
+
+    // -- Bus --
+    wire [63:0] bus_address;
+    wire [63:0] bus_write_data;
+    wire        bus_write_enable;
+    wire        bus_read_enable;
+    wire [63:0] bus_read_data;
 
 endmodule
-
-
 
 // Cyclone II FPGA Starter Board EP2C20F484C7 
 // Onchip M4K 239616~=29.95 KB
