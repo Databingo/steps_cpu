5a721dba97bb2355dcd0d850a7d5ff4f1e51442e
youhao <youhaohao@haohaohao.com>
2025-09-05 12:52:50 +0800

    update

--- cpu_on_board/cpu_on_board.v
+++ cpu_on_board/cpu_on_board.v
@@ -16,11 +16,11 @@ module cpu_on_board (
 );
 
     // -- MEM -- minic L1 cache
-    (* ram_style = "block" *) reg [31:0] Cache [0:4096]; // 2048x4=8KB L1 cache to 0x2000
+    (* ram_style = "block" *) reg [31:0] Cache [0:2047]; // 2048x4=8KB L1 cache to 0x2000
     integer i;
     initial begin
-        $readmemb("rom.mif", Cache, `Rom_base>>2, (`Rom_base+`Rom_size)>>2); // 
-        $readmemb("ram.mif", Cache, `Ram_base>>2, (`Ram_base+`Ram_size)>>2); //
+        $readmemb("rom.mif", Cache, `Rom_base>>2);
+        $readmemb("ram.mif", Cache, `Ram_base>>2);
     end
 
     // -- Clock --
