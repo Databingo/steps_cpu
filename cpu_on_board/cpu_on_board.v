ffd8a60ac0975bfb1ebb25a80b9e8d69153ae8f3
youhao <youhaohao@haohaohao.com>
2025-09-05 20:58:36 +0800

    update

--- cpu_on_board/cpu_on_board.v
+++ cpu_on_board/cpu_on_board.v
@@ -113,7 +113,7 @@ module cpu_on_board (
     wire [63:0] bus_addr;
     always @(posedge CLOCK_50) begin
         if (bus_write_enable) begin
-            Cache[bus_address[9:0]] <= bus_write_data;
+            Cache[bus_address] <= bus_write_data;
             //Cache[bus_addr] <= bus_write_data;
         end 
 	else if (bus_read_enable) begin 
