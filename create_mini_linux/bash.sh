a5bc0be005df2b77d37a7ffd4b206478ca84ca0b
root <root@localhost.localdomain>
2025-09-07 23:08:09 +0800

    update

--- create_mini_linux/bash.sh
+++ create_mini_linux/bash.sh
@@ -5,10 +5,16 @@
 #brew install gawk gnu-sed gmp mpfr libmpc isl zlib expat flock libslirp 
 #gmake ARCH=riscv CROSS_COMPILE=riscv64-unknow-linux-gun- defconfig
 #gmake ARCH=riscv CROSS_COMPILE=/usr/local/projects/bin/riscv64-lp64d--glibc--bleeding-edge-2025.08-1/bin/riscv64-buildroot-linux-gnu- defconfig
-cd riscv64-linux/linux && make ARCH=riscv CROSS_COMPILE=/usr/local/projects/bin/x86-64--glibc--bleeding-edge-2025.08-1/bin/x86_64-linux- defconfig
+#cd riscv64-linux/linux && make ARCH=riscv CROSS_COMPILE=/usr/local/projects/bin/x86-64--glibc--bleeding-edge-2025.08-1/bin/x86_64-linux- defconfig
 #git clone https://git.buildroot.net/buildroot
 #cd buildroot
 #make qemu_riscv64_virt_defconfig
 #make
 #brew install gcc
 #brew install texinfo
+#
+#
+#
+# in LXQT linux
+cd riscv64-linux/linux && make ARCH=riscv CROSS_COMPILE=/usr/local/projects/bin/x86-64--glibc--bleeding-edge-2025.08-1/bin/x86_64-linux- defconfig
+
