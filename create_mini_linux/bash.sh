7a3c041605a5f7ea3c72025617e4a86999beb4d8
root <root@localhost.localdomain>
2025-09-08 04:16:29 +0800

    update

--- create_mini_linux/bash.sh
+++ create_mini_linux/bash.sh
@@ -19,12 +19,14 @@
 #curl --output toolchain.tar.xz https://toolchains.bootlin.com/downloads/releases/toolchains/riscv64-lp64d/tarballs/riscv64-lp64d--glibc--bleeding-edge-2025.08-1.tar.xz
 #mv toolchain.tar.xz /usr/local/projects/bin && tar -xvf toolchain.tar.xz
 #sudo apt install autoconf automake autotools-dev curl libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev libexpat-dev 
-#cd riscv64-linux/linux && make ARCH=riscv CROSS_COMPILE=/usr/local/projects/bin/riscv64-lp64d--glibc--bleeding-edge-2025.08-1/bin/riscv64-buildroot-linux-gnu- defconfig
-#cp config /usr/local/projects/steps_cpu/create_mini_linux/riscv64-linux/linux/.config
+#apt-get install libncurses5-dev
+cd riscv64-linux/linux && make ARCH=riscv CROSS_COMPILE=/usr/local/projects/bin/riscv64-lp64d--glibc--bleeding-edge-2025.08-1/bin/riscv64-buildroot-linux-gnu- defconfig
+#cd riscv64-linux/linux && make ARCH=riscv CROSS_COMPILE=/usr/local/projects/bin/riscv64-lp64d--glibc--bleeding-edge-2025.08-1/bin/riscv64-buildroot-linux-gnu- manuconfig
 #cd riscv64-linux/linux && make ARCH=riscv CROSS_COMPILE=/usr/local/projects/bin/riscv64-lp64d--glibc--bleeding-edge-2025.08-1/bin/riscv64-buildroot-linux-gnu- olddefconfig
 #cd riscv64-linux/linux && make ARCH=riscv CROSS_COMPILE=/usr/local/projects/bin/riscv64-lp64d--glibc--bleeding-edge-2025.08-1/bin/riscv64-buildroot-linux-gnu- -j $(nproc)
 # git clone --depth 1 https://git.busybox.net/busybox
 #cd busybox && CROSS_COMPILE=/usr/local/projects/bin/riscv64-lp64d--glibc--bleeding-edge-2025.08-1/bin/riscv64-buildroot-linux-gnu- make defconfig && cd -
+#cd busybox && CROSS_COMPILE=/usr/local/projects/bin/riscv64-lp64d--glibc--bleeding-edge-2025.08-1/bin/riscv64-buildroot-linux-gnu- make menuconfig && cd -
 #cd busybox && CROSS_COMPILE=/usr/local/projects/bin/riscv64-lp64d--glibc--bleeding-edge-2025.08-1/bin/riscv64-buildroot-linux-gnu- make -j $(nproc) && cd -
 
 #/usr/local/projects/bin/riscv64-lp64d--glibc--bleeding-edge-2025.08-1/bin/riscv64-buildroot-linux-gnu-gcc -static init.c -o init
@@ -35,9 +37,9 @@
 ##    -drive file=busybox,format=raw,id=hd0 \
 ##    -device virtio-blk-device,drive=hd0
 
-qemu-system-riscv64 -nographic -machine virt \
-    -m 512M \
-    -bios fw_dynamic.bin \
-    -kernel Image.gz \
-    -initrd initramfs.cpio \
-    -append "root=/dev/vda ro console=ttyS0 init=/bin/sh" \
+#qemu-system-riscv64 -nographic -machine virt \
+#    -m 512M \
+#    -bios fw_dynamic.bin \
+#    -kernel Image.gz \
+#    -initrd initramfs.cpio \
+#    -append "root=/dev/vda ro console=ttyS0 init=/bin/sh" \
