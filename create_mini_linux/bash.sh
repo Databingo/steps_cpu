e1f66849d964feaa628d7bd2ff86aa3255473e65
youhao <youhaohao@haohaohao.com>
2025-09-08 22:01:52 +0800

    update

--- create_mini_linux/bash.sh
+++ create_mini_linux/bash.sh
@@ -1,4 +1,5 @@
 #qemu-system-x86_64 -cdrom image.iso
+qemu-system-x86_64 -cdrom BusyBoxLinux.iso
 #
 #
-brew install gawk gnu-sed gmp mpfr libmpc isl zlib expat
+#brew install gawk gnu-sed gmp mpfr libmpc isl zlib expat
