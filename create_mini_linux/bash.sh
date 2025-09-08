76cec9f259070832f07d3371293f0b1176720110
youhao <youhaohao@haohaohao.com>
2025-09-09 00:36:12 +0800

    update

--- create_mini_linux/bash.sh
+++ create_mini_linux/bash.sh
@@ -3,3 +3,9 @@ qemu-system-x86_64 -cdrom BusyBoxLinux.iso
 #
 #
 #brew install gawk gnu-sed gmp mpfr libmpc isl zlib expat
+#gmake ARCH=riscv CROSS_COMPILE=riscv64-unknow-linux-gun- defconfig
+#git clone https://git.buildroot.net/buildroot
+#cd buildroot
+#make qemu_riscv64_virt_defconfig
+#make
+#brew install gcc
