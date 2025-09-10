8acd4d430828cbda49770998de25d5cfce830343
youhao <youhaohao@haohaohao.com>
2025-09-10 09:22:07 +0800

    update

--- create_mini_linux/f.sh
+++ create_mini_linux/f.sh
@@ -1,6 +1,8 @@
 mkdir -p  my_rootfs/{bin,sbin,etc,proc,sys,dev}
 cd my_rootfs
 cp ../bybox  bin/
-ln -s bin/bybox  init
+cp ../init  init
+chmod +x init
+chmod +x bin/bybox
 find . | cpio -H newc -o | gzip > ../rootfs.cpio.gz 
 
