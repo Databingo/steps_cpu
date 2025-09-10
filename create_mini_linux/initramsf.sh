1ed47be37c0280bc9aac1ba7fcd4211228b5dc33
root <root@localhost.localdomain>
2025-09-10 11:22:42 +0800

    update

--- create_mini_linux/initramsf.sh
+++ create_mini_linux/initramsf.sh
@@ -1,18 +1,17 @@
-## Create initramfs 
-#rm -rf my_rootfs
-#mkdir -p my_rootfs/{bin,sbin,home,mnt,usr,tmp,etc,proc,sys,dev}
-#mkdir -p my_rootfs/usr/{bin,sbin}
-#mkdir -p my_rootfs/proc/sys/kernel
-#
-#cd my_rootfs
-#cp ../buzybox2  bin/busybox
-#cp ../init init
-#chmod +x bin/busybox
-#chmod +x init
-#
-#find . | cpio -H newc -o | gzip > ../rootfs.cpio.gz 
-#
-#
-#
-mkdir initrd
+# Create initramfs 
+rm -rf my_rootfs
+mkdir -p my_rootfs/{bin,sbin,home,mnt,usr,tmp,etc,proc,sys,dev}
+mkdir -p my_rootfs/usr/{bin,sbin}
+mkdir -p my_rootfs/proc/sys/kernel
+
+cd my_rootfs
+cp ../buzybox2  bin/busybox
+cp ../init init
+chmod +x bin/busybox
+chmod +x init
+
+find . | cpio -H newc -o | gzip > ../rootfs.cpio.gz 
+
+
+
 
