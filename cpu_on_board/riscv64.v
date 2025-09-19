248618eb0a3e9c99f46f99d4f2f1c0dffff87bb2
youhao <youhaohao@haohaohao.com>
2025-09-19 20:24:12 +0800

    update

--- cpu_on_board/riscv64.v
+++ cpu_on_board/riscv64.v
@@ -21,21 +21,17 @@ module riscv64(
 );
     // -- CSR Registers --
     reg [63:0] csr [0:4096]; // Maximal 12-bit length = 4096
-    //integer mstatus = 12'h300;      // 0x300 MRW Machine status reg   // 63_SD|37_MBE|36_SBE|35:34_SXL10|22_TSR|21_TW|20_TVW|17_MPRV|12:11_MPP10|7_MPIE|3_MIE|1_SIE|0_WPRI
+    integer mstatus = 12'h300;      // 0x300 MRW Machine status reg   // 63_SD|37_MBE|36_SBE|35:34_SXL10|22_TSR|21_TW|20_TVW|17_MPRV|12:11_MPP10|7_MPIE|3_MIE|1_SIE|0_WPRI
     integer mie = 12'h304;          // 0x304 MRW Machine interrupt-enable register *
     integer mip = 12'h344;          // 0x344 MRW Machine interrupt pending *
     integer mtvec = 12'h305;        // 0x305 MRW Machine trap-handler base address *
     integer mcause = 12'h342;       // 0x342 MRW Machine trap casue *
-    integer mepc = 12'h341;         // 0x341 MRW Machine exception program counter *
-
+    integer mepc_ = 12'h341;         // 0x341 MRW Machine exception program counter *
     // -- CSR Bits --
-    //wire mstatus_MIE = csr[mstatus][3]; // Machine Interrupt Enable
-    wire mstatus_MIE = mstatus[3]; // Machine Interrupt Enable
-    //wire mstatus_MPIE = csr[mstatus][7]; // 
-    //wire mie_MEIE = csr[mie][11];
-    //wire mip_MEIP = csr[mie][11];
-    //reg [31:0] mepc; // Machine Exception Program Counter
-    reg [63:0] mstatus; // Machine Exception Program Counter
+    wire mstatus_MIE = csr[mstatus][3];
+    wire mie_MEIE = csr[mie][11];
+    wire mip_MEIP = csr[mie][11];
+    reg [31:0] mepc; //csr?
  
     // -- Immediate decoders  -- 
     wire signed [63:0] w_imm_u = {{32{ir[31]}}, ir[31:12], 12'b0};
@@ -47,12 +43,11 @@ module riscv64(
     reg lb_step;
     reg sb_step;
 
-    // IF
+    // IF ir (Unchanged)
     always @(posedge clk or negedge reset) begin
         if (!reset) begin 
             heartbeat <= 1'b0; 
             ir <= 32'h00000001; 
-	    mstatus[3] <= 1'b1;
         end else begin
             heartbeat <= ~heartbeat; // heartbeat
             ir <= instruction;
@@ -71,7 +66,7 @@ module riscv64(
 	    bus_write_data <= 0;
 	    bus_address <= `Ram_base;
             // Interrupt reset
-	    //interrupt_pending <= 0;
+	    interrupt_pending <= 0;
 	    interrupt_ack <= 0;
 
         end else begin
@@ -80,21 +75,11 @@ module riscv64(
 	    interrupt_ack <= 0;
 
             // Interrupt
-	    //if (interrupt_vector == 1 && interrupt_pending !=1) begin
-	    if (interrupt_vector == 1 && mstatus_MIE == 1) begin
-	        //mepc <= pc; // save pc
-	        csr[mepc] <= pc; // save pc
-		csr[mcause] <= 64'h800000000000000B; // MSB 1 for Interrupts 0 for exceptions, Casue = 11 (Machine External Interrupt)
-		//csr[mstatus][7] <= csr[mstatus][3]; // mstatus.MPIC = mstatus.MIE
-		//csr[mstatus][3] <= 1'b0; //mstatus.MIE = 0
-		mstatus[7] <= mstatus[3]; // mstatus.MPIC = mstatus.MIE
-		mstatus[3] <= 1'b0; //mstatus.MIE = 0
-		//pc <= {csr[mtvec][63:2], 2'b00} // jump to dispatch handler(which save contect to stack & read mcause to choose ISR)
+	    if (interrupt_vector == 1 && interrupt_pending !=1) begin
+	        mepc <= pc; // save pc
                 pc <= 0; // jump to ISR addr
-
-
 		bubble <= 1'b1; // bubble wrong fetched instruciton by IF
-	        //interrupt_pending <= 1;
+	        interrupt_pending <= 1;
 		interrupt_ack <= 1; // reply to outside
 
             // Bubble
@@ -109,14 +94,7 @@ module riscv64(
                 casez(ir) 
 	            32'b???????_?????_?????_???_?????_0110111:  re[w_rd] <= w_imm_u; // Lui
 	            32'b0000000_00000_00000_000_00000_0000000: begin // Mret 
-	                //pc <= mepc; bubble <= 1; interrupt_pending <= 0; end 
-		        //csr[mstatus][3] <= csr[mstatus][7]; // mstatus.MIE = mstatus.MPIC 
-		        //csr[mstatus][7] <= 1'b1; //mstatus.MIE = 1
-		        //mstatus[3] <= mstatus[7]; // mstatus.MIE = mstatus.MPIC 
-		        //mstatus[7] <= 1'b1; //mstatus.MIE = 1
-			//pc <= csr[mepc];
-			bubble <= 1; 
-		        end
+	                pc <= mepc; bubble <= 1; interrupt_pending <= 0; end 
 	            32'b1111111_11111_11111_111_11111_1111111: begin // Load  3 cycles to finish re<=data
 	                if (lb_step == 0) begin
 	                    bus_address <= `Key_base; // cycle 1 setting read enable
