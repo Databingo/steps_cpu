6f8e1035e70c89974eb3fd9556885d1d1fbb7dc8
youhao <youhaohao@haohaohao.com>
2025-09-23 10:09:17 +0800

    update

--- cpu_on_board/riscv64.v
+++ cpu_on_board/riscv64.v
@@ -81,9 +81,7 @@ module riscv64(
 	end
     endfunction
 
-
     // -- Innerl signal --
-    //reg interrupt_pending;  
     reg bubble;
     reg lb_step;
     reg sb_step;
@@ -112,7 +110,6 @@ module riscv64(
 	    bus_address <= `Ram_base;
             // Interrupt re-enable
 	    csr_mstatus[MIE] <= 1;
-	    //interrupt_pending <= 0;
 	    interrupt_ack <= 0;
 
         end else begin
@@ -121,21 +118,15 @@ module riscv64(
 	    interrupt_ack <= 0;
 
             // Interrupt
-	    //if (interrupt_vector == 1 && interrupt_pending !=1) begin
-	    //if (interrupt_vector == 1 && csr_bit(mstatus, MIE) ==1) begin //mstatus[3] MIE
 	    if (interrupt_vector == 1 && mstatus_MIE == 1) begin //mstatus[3] MIE
-	    //if (interrupt_vector == 1 && csr_read(mstatus)[MIE] ==1) begin //mstatus[3] MIE
-	        //mepc <= pc; // save pc
 	        csr_mepc <= pc; // save pc
 
 		csr_mcasue <= 64'h800000000000000B; // MSB 1 for interrupts 0 for exceptions, Cause 11 for Machine External Interrupt
 		csr_mstatus[MPIE] <= csr_mstatus[MIE];
 		csr_mstatus[MIE] <= 0;
 
-                //pc <= 0; // jump to ISR addr
 		pc <= csr_mtvec; // jump to mtvec addrss (default 0, need C or Assembly code of handler)
 		bubble <= 1'b1; // bubble wrong fetched instruciton by IF
-	        //interrupt_pending <= 1;
 	        csr_mstatus[MIE] <= 0;
 		interrupt_ack <= 1; // reply to outside
 
@@ -150,49 +141,25 @@ module riscv64(
 	        bus_address <= `Ram_base;
                 casez(ir) 
 	            32'b???????_?????_?????_???_?????_0110111:  re[w_rd] <= w_imm_u; // Lui
-	            //32'b0000000_00000_00000_000_00000_0000000: begin // Mret 
 	            32'b0011000_00010_?????_000_?????_1110011: begin   // Mret
-	                //pc <= mepc; 
 	                pc <= csr_read(mepc); 
 			bubble <= 1; 
-			//interrupt_pending <= 0; 
 		        csr_mstatus[MIE] <= csr_mstatus[MPIE];
 		        csr_mstatus[MPIE] <= 1;
 		    end 
-	            //32'b1111111_11111_11111_111_11111_1111111: begin // Load  3 cycles to finish re<=data
-	            //    if (lb_step == 0) begin
-	            //        bus_address <= `Key_base; // cycle 1 setting read enable
-	            //        bus_read_enable <= 1;
-	            //        pc <= pc - 4;
-	            //        bubble <= 1; //!! take over cycle 2, meanwhile bus read 
-	            //        lb_step <= 1;
-	            //    end
-	            //    if (lb_step == 1) begin  
-	            //        re[5]<= bus_read_data; // cycle 3 save to cpu's register
-	            //        lb_step <= 0;
-	            //    end
-	            //end
-		    // Real LD
-		    // imm[11:0] | rs1 | func3=011 | rd | opcode=0000011
-		    32'b???????_?????_?????_011_?????_0000011: begin 
+		    32'b???????_?????_?????_011_?????_0000011: begin  // Ld
 	                if (lb_step == 0) begin
-	                    //bus_address <= `Key_base; // cycle 1 setting read enable
 	                    bus_address <= re[w_rs1] + w_imm_i ;
-
 	                    bus_read_enable <= 1;
 	                    pc <= pc - 4;
 	                    bubble <= 1; //!! take over cycle 2, meanwhile bus read 
 	                    lb_step <= 1;
 	                end
 	                if (lb_step == 1) begin  
-	                    //re[5]<= bus_read_data; // cycle 3 save to cpu's register
 	                    re[w_rd]<= bus_read_data; // cycle 3 save to cpu's register
 	                    lb_step <= 0;
 	                end
-		    //mem_addr <= l_addr; re[w_rd] <= mem_data_in; 
-		    end // Ld
-
-
+		    end 
 	            //32'b1111111_11111_11111_111_11110_1111111: begin // Store 1 cycles to finish settting data<=re (next cycle bus write to data)
 	            32'b???????_?????_?????_011_?????_0100011: begin // Sd
 		        if (sb_step == 0) begin 
