75201438bf8c100149dac05b9067036a5b4e44ce
youhao <youhaohao@haohaohao.com>
2025-09-25 15:54:46 +0800

    update

--- cpu_on_board/test.sh
+++ cpu_on_board/test.sh
@@ -4,7 +4,8 @@ cat bin.txt > rom.mif  # write ROM program
 
 #go run ../rvas.go test.s
 #go run ../rvas.go addi.s
-go run ../rvas.go jal.s
+#go run ../rvas.go jal.s
+go run ../rvas.go jal_ra.s
 cat bin.txt > ram.mif  # write RAM program
 
 
