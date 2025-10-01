ae4978d2bc3c3fd01fe28e77be9a42333da54718
youhao <youhaohao@haohaohao.com>
2025-10-01 21:14:04 +0800

    update

--- cpu_on_board/cpu_on_board.v
+++ cpu_on_board/cpu_on_board.v
@@ -157,6 +157,10 @@ module cpu_on_board (
     always @(posedge CLOCK_50) begin
         //read_address_reg <= bus_address>>2;
         bus_read_data <= {32'd0, Cache[bus_address>>2]};
+    end
+    always @(posedge CLOCK_50) begin
+        //read_address_reg <= bus_address>>2;
+        //bus_read_data <= {32'd0, Cache[bus_address>>2]};
         if (bus_read_enable) begin
            if (Key_selected) bus_read_data <= {32'd0, 24'd0, ascii};
            //if (Ram_selected) bus_read_data <= {32'd0, Cache[read_address_reg]};
