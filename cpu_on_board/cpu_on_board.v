e7e3f4914d29cf7510b051c4d757a0310a24a534
youhao <youhaohao@haohaohao.com>
2025-10-01 20:29:10 +0800

    update

--- cpu_on_board/cpu_on_board.v
+++ cpu_on_board/cpu_on_board.v
@@ -159,7 +159,7 @@ module cpu_on_board (
         read_address_reg <= bus_address_read>>2;
         if (bus_read_enable) begin
            if (Key_selected) bus_read_data <= {32'd0, 24'd0, ascii};
-           if (Ram_selected) bus_read_data <= {32'd0, Cache[bus_address_read>>2]};
+           if (Ram_selected) bus_read_data <= {32'd0, Cache[read_address_reg]};
         end
     end
       
