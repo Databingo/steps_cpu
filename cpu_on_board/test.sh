71f37dd8f7e3b5542750145fb0b0add0caac2209
youhao <youhaohao@haohaohao.com>
2025-10-02 20:47:57 +0800

    update

--- cpu_on_board/test.sh
+++ cpu_on_board/test.sh
@@ -13,8 +13,8 @@ cat bin.txt > rom.mif  # write ROM program
 #go run ../rvas.go jalr.s
 #go run ../rvas.go jal.s
 #go run ../rvas.go j_jr_ret.s  #
-go run ../rvas.go auipc.s
-#go run ../rvas.go call.s #
+#go run ../rvas.go auipc.s
+go run ../rvas.go call.s #
 #go run ../rvas.go sd_ld.s
 #go run ../rvas.go sd.s
 #go run ../rvas.go jal_ra.s
