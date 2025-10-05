c193f2ebba27782fdddbe61b6bf280424100d9a5
youhao <youhaohao@haohaohao.com>
2025-10-05 19:36:57 +0800

    update

---
+++ cpu_on_board/ld.s
@@ -0,0 +1,48 @@
+# RISC-V Assembly: Ld 
+
+.section .text
+.globl _start
+
+_start:
+    # --- Setup Phase ---
+    # Get UART address (0x2004) into t0.
+    lui t0, 0x2
+    addi t0, t0, 4      # t0 = 0x2004
+
+    # --- Test 1: Basic Shift ---
+    # We expect t1 = 0x5373415050417353 SsAPPAsS
+    lui  t1, 0x53734
+    addi t1, t1, 0x150
+    slli t1, t1, 8   
+    addi t1, t1, 0x50 
+    slli t1, t1, 8   
+    addi t1, t1, 0x41 
+    slli t1, t1, 8   
+    addi t1, t1, 0x73 
+    slli t1, t1, 8   
+    addi t1, t1, 0x53 
+   
+    sd t1, -16(t0) # save
+    ld t2, -16(t0) # load back to t2
+
+    srli t4, t2, 24 # P
+    sb t4, 0(t0) 
+    srli t4, t2, 16 # A
+    sb t4, 0(t0)
+    srli t4, t2, 8  # s
+    sb t4, 0(t0)
+    sb t2, 0(t0)    # S
+
+
+    srli t4, t2, 56 # S
+    sb t4, 0(t0) 
+    srli t4, t2, 48 # s
+    sb t4, 0(t0)
+    srli t4, t2, 40  # A
+    sb t4, 0(t0)
+    srli t4, t2, 32  # P
+    sb t4, 0(t0)    
+
+
+
+
--- cpu_on_board/test.sh
+++ cpu_on_board/test.sh
@@ -13,7 +13,8 @@ cat bin.txt > rom.mif  # write ROM program
 #go run ../rvas.go sh.s       # lui addi sh lw sb uart for test
 #go run ../rvas.go lh.s        # lui addi sw lh sb uart for test
 #go run ../rvas.go slli.s      # lui addi slli sb uart
-go run ../rvas.go sd.s        # lui addi slli addi sd lw sb uart for test
+#go run ../rvas.go sd.s        # lui addi slli addi sd lw sb uart for test
+go run ../rvas.go ld.s        # lui addi slli addi sd ld sb uart for test
 #
 #
 #
