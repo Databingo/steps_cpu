1d79d8dc4c7333a3b885116220b3acdda4391984
youhao <youhaohao@haohaohao.com>
2025-10-05 09:56:29 +0800

    update

--- cpu_on_board/lb.s
+++ cpu_on_board/lb.s
@@ -14,12 +14,12 @@ _start:
     addi t0, t0, 2047
     addi t0, t0, 8      # t0 = 8196 (0x2004)
     
-    addi t1, x0, 0x80
-    sb t1, -16(t0)        # Store 0x80 to address 0x2004-16.
-    lb t2, -16(t0)        # Load 0x80 to t2
+    addi t1, x0, 80
+    sb t1, -16(t0)        # Store 80 P to address 0x2004-16.
+    lb t2, -16(t0)        # Load 80 to t2
     sb t2, 0(t0)          # Print  
 
-    addi t1, x0, 0x41
-    sb t1, -15(t0)        # Store 0x41 to address 0x2004-15.
-    lb t2, -15(t0)        # Load 0x41 to t2
+    addi t1, x0, 41
+    sb t1, -15(t0)        # Store 41 A to address 0x2004-15.
+    lb t2, -15(t0)        # Load 41 to t2
     sb t2, 0(t0)          # Print  
