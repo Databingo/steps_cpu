96e21b6e7bb78422d42f5270bf0a35f87bf76875
youhao <youhaohao@haohaohao.com>
2025-10-05 17:46:43 +0800

    update

--- cpu_on_board/lh.s
+++ cpu_on_board/lh.s
@@ -7,11 +7,8 @@ _start:
     # --- Setup Phase ---
     # Get the UART address (0x2004) into t3.
     #    (Using a sequence of addi instructions as before)
-    addi t0, x0, 2047
-    addi t0, t0, 2047
-    addi t0, t0, 2047
-    addi t0, t0, 2047
-    addi t0, t0, 8      # t3 = 8196 (0x2004)
+    lui t0, 0x2
+    addi t0, t0, 4      # t0 = 8196 (0x2004)
 
 
     # --- Action Phase ---
@@ -21,21 +18,16 @@ _start:
 
     sw t1, -16(t0)   # save t1 value to ram -16(t0) aligned
     addi t1, x0, 0   # clean t1 to 0
-    lh t1, -16(t0)    # loab back low16
-    lh t7, -14(t0)    # loab back high16
+    lh t1, -16(t0)    # loab back low16 'sS'
+    lh t6, -14(t0)    # loab back high16 'PA'
 
 
-    # -- Print 'P' --
-    srli t2, t1, 24     # Isolate 'P' (0x50)
-    sb t2, 0(t0)        # Print 'P'
+    # -- Print 'PA' --
+    srli t4, t6, 8     
+    sb t4, 0(t0)        # Print 'P' 
+    sb t6, 0(t0)        # Print 'A'
     
-    # -- Print 'A' --
-    srli t3, t1, 16     # Isolate 'A' (0x41)
-    sb t3, 0(t0)        # Print 'A'
-    
-    # -- Print 'S' --
-    srli t4, t1, 8      # Isolate 's' (0x73)
-    sb t4, 0(t0)        # Print 's'
-    
-    # -- Print final 'S' --
-    sb t1, 0(t0)        # Print the lowest byte 'S'
+    # -- Print 'sS' --
+    srli t2, t1, 8      
+    sb t2, 0(t0)        # Print 's'
+    sb t1, 0(t0)        # Print 'S'
