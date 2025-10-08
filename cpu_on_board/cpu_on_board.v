28710d2fdd726d8686e67621c746b8e821d8b438
youhao <youhaohao@haohaohao.com>
2025-10-08 23:40:29 +0800

    update

--- cpu_on_board/cpu_on_board.v
+++ cpu_on_board/cpu_on_board.v
@@ -146,6 +146,8 @@ module cpu_on_board (
     //wire [8:0] tlb0 = vpn[8:0];
     reg tlb_hit;
     reg valid;
+    reg sfentce;
+    reg priv_s;
     parameter PAGE_OFFSET_BITS = 12;
     parameter VPN_BITS = 20;
     parameter TLB_ENTRIES = 8; //small tlb
@@ -159,13 +161,29 @@ module cpu_on_board (
     integer i;
     reg [2:0] tlb_replace_index;
 
-
-
-
-
     always @(posedge CLOCK_50) begin
-        if (bus_read_enable) begin 
-	mmu <= 1
+	if (!reset) begin
+	    valid <=0; tlb_hit<=0,tlb_replace_index<=0;
+	    for (i=0;i<TLB_ENTRIES;i=i+1) tlb_valid[i]<=0;
+	end else if (sfence) begin
+	    for (i=0;i<TLB_ENTRIES;i=i+1) tlb_valid[i]<=0; // Flush all TLB entries
+	end else begin
+	    valid <=0;
+	    tlb_hit<=0; // Search
+	    for (i=0;i<TLB_ENTRIES;i=i+1) begin
+		if (tlb_valid[i] && tlb_vpn[i]==vpn) begin
+		    if (!priv_s && !tlb_u[i]) begin
+			valid <=0;
+		    end	else begin
+			pa <= {tlb_ppn[i], offset}
+			valid <= 1;
+			tlb_hit <=1;
+	                mmu <= 1
+		    end
+	        end
+	    end
+	    if (!tlb_hit) begin
+	    end
         end
     end
       
