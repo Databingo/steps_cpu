9f85d8b47ad4cb97e92632d650f61d5433d58926
youhao <youhaohao@haohaohao.com>
2025-10-24 15:45:29 +0800

    update

--- cpu_on_board/cpu_on_board.v
+++ cpu_on_board/cpu_on_board.v
@@ -204,10 +204,7 @@ module cpu_on_board (
 	    if (Ram_selected) begin bus_read_data <= {32'd0, Cache[bus_address_reg]}; bus_read_done <= 1; end
 	    // Sd read
 	    if (Sdc_ready_selected) begin
-		case (sd_read_step)
-		    0: begin mem_a <= `Sdc_ready; sd_read_step <= 1; end
-		    1: begin bus_read_data <= {32'd0, spo};sd_read_step <= 0;bus_read_done <= 1;end
-		endcase
+		    mem_a <= `Sdc_ready; bus_read_data <= {32'd0, spo}; bus_read_done <= 1;
 	    end
 	    if (Sdc_cache_selected) begin
 		case (sd_read_step)
