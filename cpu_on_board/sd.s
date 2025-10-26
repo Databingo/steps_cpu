6465c46b9e3e842cbe06a0713962404bd2dd68e6
youhao <youhaohao@haohaohao.com>
2025-10-26 09:46:51 +0800

    update

--- cpu_on_board/sd.s
+++ cpu_on_board/sd.s
@@ -8,15 +8,45 @@
 _start:
 
 # Get UART address (0x2004) into t6.
-lui t6, 0x2
-addi t6, t6, 4      # t0 = 0x2004
+lui t0, 0x2
+addi t0, t0, 4      # t0 = 0x2004
+
+# SD base
+lui a1, 0x3         # a1 = 0x3000 base
+
 
 sd_ready:
-lui a1, 0x3
-addi a1, a1, 0x220  # ready map
-lw a2, 0(a1)
-addi a3, x0, 1
-bne a2, a3, sd_ready
+lw a2, 0x220(a1)    # a2 0x3220 ready
+beq a2, x0, sd_ready
 
 addi t1, x0, 65  # A
-sw t1, 0(t6) # print
+sw t1, 0(t0) # print
+
+
+# Sector address value to address 0x3200
+sw x0, 0x200(a1)
+
+# Trigger read at 0x3204
+addi a3, x0, 1
+sw a3, 0x204(a1)
+
+sd_ready_2:
+lw a2, 0x220(a1)    # a2 0x3220 ready
+beq a2, x0, sd_ready_2
+
+lw t2, a1           # load first byte at 0x3000
+andi t2, t2, 0xFF   # Isolate byte value
+
+srli t3, t2, 4      # get high nibble
+addi t3, t3, 55     # 10 is "A" ascii 65 ..
+sw t3, 0(t0)
+
+andi t4, t2, 0x0F      # get low nibble
+addi t4, t4, 55        # 10 is "A" ascii 65 ..
+sw t4, 0(t0)
+
+
+
+
+
+
