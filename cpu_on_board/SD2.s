42269d01bcbff1b2c264108c0b723164f036ef4f
youhao <youhaohao@haohaohao.com>
2025-11-02 16:21:07 +0800

    update

--- cpu_on_board/SD2.s
+++ cpu_on_board/SD2.s
@@ -8,16 +8,6 @@
 # UART 0x2004
 
 # Boot Sector: Sector 0 LBA
-# BPB (BIOS Parameter Block) in sector 0
-offset size FAT16 FAT32 filed 
-# 0x0b 2    512   512   bytes per secter
-# 0x0d 2                sectors per cluster 
-# 0x0e 2    1     32    reserverd sectors
-# 0x10 1    2     2     numbers of FATs
-# 0x11 2    512   0     root entries
-# 0x16 2    9     0     sectors per FAT
-# 0x24 4    0     0x9f0 sectors per FAT (fat32 only)
-# 0x36 8    FAT16 FAT32 FAT label string 
 # root_dir_sector = reserved_sectors + (num_FATs * sectors_per_FAT)
 
 
@@ -39,10 +29,10 @@ beq a2, x0, sd_ready
 addi t1, x0, 65  # A
 sw t1, 0(t0)     # print
 
-# Trigger read of sector 0 at 0x3204
-# Sector address value to address 0x3200
+# Write Sector index value to address 0x3200
 sw x0, 0x200(a1)
-addi a3, x0, 1
+li a3, 1
+# Trigger read at 0x3204
 sw a3, 0x204(a1)
 
 sd_ready_2:
@@ -52,12 +42,65 @@ beq a2, x0, sd_ready_2
 addi t1, x0, 66  # B
 sw t1, 0(t0)     # print
 
+# Wait for cache available
 avail:
 lw a2, 0x228(a1)    # a2 0x3228 cache_avaible
 beq a2, x0, avail
 
 addi t1, x0, 67  # C
 sw t1, 0(t0)     # print
+
+
+# Parse BPB t6a7s11
+
+# BPB (BIOS Parameter Block) in sector 0
+# offset size FAT16 FAT32 filed 
+# 0x0b 2    512   512   bytes per secter
+# 0x0d 2                sectors per cluster 
+# 0x0e 2    1     32    reserverd sectors
+# 0x10 1    2     2     numbers of FATs
+# 0x11 2    512   0     root entries
+# 0x16 2    9     0     sectors per FAT
+# 0x24 4    0     0x9f0 sectors per FAT (fat32 only)
+# 0x2c                  root cluster (fat32 only)
+# 0x36 8    FAT16 FAT32 FAT label string 
+
+addi t1, a1, 0x0B
+lw t2, 0(t1)
+andi t2, t2, 0xffff
+mv s0, t2    # s0 = bytes_per_sector
+
+addi t1, a1, 0x0E
+lw t2, 0(t1)
+andi t2, t2, 0xffff
+mv s1, t2    # s1 = reserved
+
+addi t1, a1, 0x10
+lw t2, 0(t1)
+andi t2, t2, 0xffff
+mv s2, t2    # s2 = num_fats
+
+addi t1, a1, 0x11
+lw t2, 0(t1)
+andi t2, t2, 0xffff
+mv s3, t2    # s3 = root_entries
+
+addi t1, a1, 0x16
+lw t2, 0(t1)
+andi t2, t2, 0xffff
+mv s4, t2    # s4 = sectors_per_fat16
+
+addi t1, a1, 0x2C
+lw t2, 0(t1)
+mv s5, t2    # s5 = root_cluster
+
+# Detect FAT type
+beq s3, x0, is_fat32  # root_entries == 0
+beq s4, x0, is_fat32  #  sectors_per_fat == 0
+
+
+
+
 
 li t1, 0   # byte index
 li t6, 511 # max byte index
