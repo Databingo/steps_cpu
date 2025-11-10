16f1dc32c7f8f9a4a227a5633f0bf4334ea6c7fa
root <root@localhost.localdomain>
2025-11-10 16:53:39 +0800

    update

--- cpu_on_board/clear.sh
+++ cpu_on_board/clear.sh
@@ -1,14 +1,10 @@
 #sudo sync
-sudo sh -c 'echo 3 > /proc/sys/vm/drop_caches'
+#sudo sh -c 'echo 3 > /proc/sys/vm/drop_caches'
 #sudo systemctl stop cups avahi-daemon bluetooth
 
 
 #sudo apt update
 #sudo apt install qemu-guest-agent
 #sudo systemctl enable --now qemu-guest-agent
-<<<<<<< HEAD
-=======
 systemctl start qemu-guest-agent
 systemctl status qemu-guest-agent
-
->>>>>>> 688ec555d2cc34ad3e5970057b33fe9a57b1c9c7
