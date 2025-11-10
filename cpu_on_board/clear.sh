688ec555d2cc34ad3e5970057b33fe9a57b1c9c7
youhao <youhaohao@haohaohao.com>
2025-11-10 16:40:58 +0800

    update

--- cpu_on_board/clear.sh
+++ cpu_on_board/clear.sh
@@ -3,6 +3,9 @@
 #sudo systemctl stop cups avahi-daemon bluetooth
 
 
-sudo apt update
-sudo apt install qemu-guest-agent
-sudo systemctl enable --now qemu-guest-agent
+#sudo apt update
+#sudo apt install qemu-guest-agent
+#sudo systemctl enable --now qemu-guest-agent
+systemctl start qemu-guest-agent
+systemctl status qemu-guest-agent
+
