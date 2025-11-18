fef61d6c7d99c9e4311f7ae2c77cf215b228fdef
youhao <youhaohao@haohaohao.com>
2025-11-18 08:23:32 +0800

    update

--- cpu_on_board/cpu_on_board.v
+++ cpu_on_board/cpu_on_board.v
@@ -323,8 +323,8 @@ assign DRAM_CKE = 1; // always enable
 			   0: begin sdram_addr <= bus_address[22:1]; sdram_read_en <= 1; sdram_byte_en <= bus_address[0] ? 2'b10 : 2'b01; step <= 1; end
 	                   1: begin if (sdram_req_wait==0) begin 
 			       case(bus_address[0])
-				   0: begin bus_read_data <= {48'b0, sdram_rddata[7:0]}; bus_read_done <= 1; step <= 0; end  // byte 01
-			           1: begin bus_read_data <= {48'b0, sdram_rddata[15:8]}; bus_read_done <= 1; step <= 0; end // byte 10
+				   0: begin bus_read_data <= {56'b0, sdram_rddata[7:0]}; bus_read_done <= 1; step <= 0; end  // byte 01
+			           1: begin bus_read_data <= {56'b0, sdram_rddata[15:8]}; bus_read_done <= 1; step <= 0; end // byte 10
 			       endcase
 			   end end
 		        endcase
@@ -374,20 +374,20 @@ assign DRAM_CKE = 1; // always enable
 	    if (Art_selected) begin uart_write_pulse <= 1; bus_write_done <=1; end
 
 	    //if (Sdram_selected) begin if (sdram_req_wait==0) bus_write_done <= 1; end
-	    sdram_write_en <= 0;
+	    //sdram_write_en <= 0;
 	    if (Sdram_selected) begin 
 		case(bus_ls_type)
 	            3'b000: begin
 			case(step)
 			   0: begin sdram_addr <= bus_address[22:1]; sdram_wrdata <= bus_write_data[15:0]; sdram_write_en <= 1; step <= 1; sdram_byte_en <= bus_address[0] ? 2'b10 : 2'b01; end
-	                   1: begin if (sdram_req_wait==0) begin bus_write_done <= 1; step <= 0; end end // sb000 
+	                   1: begin if (sdram_req_wait==0) begin bus_write_done <= 1; sdram_write_en <= 0; step <= 0; end end // sb000 
 			//if (sdram_req_wait==0) bus_write_done <= 1; end
 		        endcase
 		    end
 		    3'b001: begin
 			case(step)
 			   0: begin sdram_addr <= bus_address[22:1]; sdram_wrdata <= bus_write_data[15:0]; sdram_write_en <= 1; step <= 1; sdram_byte_en <= 2'b11; end
-	                   1: begin if (sdram_req_wait==0) begin bus_write_done <= 1; step <= 0; end end // sh001 
+	                   1: begin if (sdram_req_wait==0) begin bus_write_done <= 1; sdram_write_en <= 0; step <= 0; end end // sh001 
 		        endcase
 		    end
 	        endcase
