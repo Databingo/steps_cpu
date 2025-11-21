ce8b7c2df37eb681298e5e716a996afdec8f08db
youhao <youhaohao@haohaohao.com>
2025-11-21 11:58:03 +0800

    update

--- cpu_on_board/mmu_d.v
+++ cpu_on_board/mmu_d.v
@@ -14,15 +14,68 @@ module mmu(
     input wire rst,
     input wire [63:0] satp,
     input wire [63:0] va,
+    //input wire start, 
+
+
+
+    output reg mmu_walking, // bus_read_enable || bus_write_enable
     output reg [63:0] pa
+    //output reg done
 );
+
+wire [8:0] vpn [0:2];
+assign vpn[2] = va[38:30]; // offset in page
+assign vpn[1] = va[29:21];
+assign vpn[0] = va[20:12];
+
+reg [1:0] level;
+reg [43:0] active_ppn;
+reg state; // 0=IDLE; 1=WALKING
+
+
+
+
+
 always @(posedge clk or negedge rst) begin
     if (!rst) 
 	pa <= 0;
+        mmu_walking <= 0;
+	state <= 0;
+	level <= 2;
+	active_ppn <= 0;
     else begin
-	pa <= va +1;
+	if (mmu_walking) begin
+	    case(state) begin
+		0: begin
+                   pa <= va +1;
+		   active_ppn <= satp[43:0]; // load Root Table
+		   level <= 2; // vpn2
+		   state <=1;
+	        end
+		1: begin
+		   addr <= {active_ppn, 12'b0} + (vpn[level] << 3); // addr = (PPN * 4096) + (vpn[level] * 8)
+		   data <= sdram[addr];
+		   if data[3;1] !=0 || level == 0) begin
+		       pa <= {data[53:10], va[11:0]};
+		       state <= 0; // done
+		       mmu_walking <= 0;
+		   end else begin
+		       active_ppn <= data[53:10];
+		       level <= level-1;
+		   end
+		end
+	    endcase
+	end
     end
 end
+
+
+
+
+
+
+
+
 
 `ifdef TEST
     initial begin
