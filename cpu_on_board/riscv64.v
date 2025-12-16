38070fdccc6b935f2894b0631e1630034c6eff91
youhao <youhaohao@haohaohao.com>
2025-12-16 11:32:34 +0800

    update

--- cpu_on_board/riscv64.v
+++ cpu_on_board/riscv64.v
@@ -325,7 +325,7 @@ module riscv64(
     //end
 
     // IF Instruction (Only drive IR)
-    reg [31:0] ir_shadow,
+    reg [31:0] ir_shadow;
     always @(posedge clk or negedge reset) begin
             if (mmu_pc || mmu_da || mmu_cache_refill) ir_shadow <= get_shadow_ir(pc);  // Runing shadow code
     end
