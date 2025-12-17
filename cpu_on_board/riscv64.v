870c10dad370d184722aa0fe65b94cd771de2cce
youhao <youhaohao@haohaohao.com>
2025-12-17 20:53:18 +0800

    update

--- cpu_on_board/riscv64.v
+++ cpu_on_board/riscv64.v
@@ -377,8 +377,8 @@ module riscv64(
 	    end else if (satp_mmu && !mmu_pc && !mmu_da && tlb_i_hit && !tlb_d_hit && (op == 7'b0000011 || op == 7'b0100011 || op == 7'b0101111) ) begin  
 	    //end else if (satp_mmu && !mmu_pc && !mmu_da && !tlb_d_hit && (op == 7'b0000011 || op == 7'b0100011 || op == 7'b0101111) ) begin  
 		mmu_da <= 1; // MMU_DA ON
-		//pc <= 28; // D-TLB refill Handler
-		pc <= 0; // D-TLB refill Handler
+		pc <= 28; // D-TLB refill Handler
+		//pc <= 0; // D-TLB refill Handler
 	 	bubble <= 1'b1; // bubble
 	        saved_user_pc <= pc - 4; // save pc EXE l/s/a
 		for (i=0;i<10;i=i+1) begin sre[i]<= re[i]; end // save re
