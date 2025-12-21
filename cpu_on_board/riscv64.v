fed38cee9f0b4f33fca53322b067ed5eafdfb74c
youhao <youhaohao@haohaohao.com>
2025-12-21 19:16:11 +0800

    update

--- cpu_on_board/riscv64.v
+++ cpu_on_board/riscv64.v
@@ -205,8 +205,8 @@ module riscv64(
     //wire [63:0] ls_va = (op == 7'b0000011) ? (rs1 + w_imm_i) : (op == 7'b0100011) ? (rs1 + w_imm_s) : (op == 7'b0101111) ? rs1 : 0; // load/jalr/store/atom
     //wire [63:0] pda = ls_va;
 
-   //(* keep = 1 *) wire [63:0] ls_va = (op == 7'b0000011) ? (rs1 + w_imm_i) : (op == 7'b0100011) ? (rs1 + w_imm_s) : (op == 7'b0101111) ? rs1 : 64'h0; // load/jalr/store/atom
-   (* keep = 1 *) reg [63:0] ls_va;
+   (* keep = 1 *) wire [63:0] ls_va = (op == 7'b0000011) ? (rs1 + w_imm_i) : (op == 7'b0100011) ? (rs1 + w_imm_s) : (op == 7'b0101111) ? rs1 : 64'h0; // load/jalr/store/atom
+   //(* keep = 1 *) reg [63:0] ls_va;
     wire [63:0] pda;
 
     //wire [26:0] data_vpn = ls_va[38:12];
@@ -216,7 +216,7 @@ module riscv64(
 	 ls_va = 64'h0;
          tlb_d_hit = 0;
          data_ppn = 44'h0;
-	 ls_va = (op == 7'b0000011) ? (rs1 + w_imm_i) : (op == 7'b0100011) ? (rs1 + w_imm_s) : (op == 7'b0101111) ? rs1 : 64'h0; // load/jalr/store/atom
+	 //ls_va = (op == 7'b0000011) ? (rs1 + w_imm_i) : (op == 7'b0100011) ? (rs1 + w_imm_s) : (op == 7'b0101111) ? rs1 : 64'h0; // load/jalr/store/atom
 	 //if (op == 7'b0000011)  ls_va = rs1 + w_imm_i;
 	 //if (op == 7'b0100011)  ls_va = rs1 + w_imm_s;
 	 //if (op == 7'b0101111)  ls_va = rs1;
