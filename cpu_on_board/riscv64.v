aefe565d12de1bb6ed6e307b9f84eb896e98648a
youhao <youhaohao@haohaohao.com>
2025-12-28 10:16:03 +0800

    update

--- cpu_on_board/riscv64.v
+++ cpu_on_board/riscv64.v
@@ -353,10 +353,6 @@ module riscv64(
 
 	    end else if (tlb && !mmu_da && !mmu_pc) begin
 		//if hit
-         //     if      (tlb_vld[0] && tlb_vpn[0] == data_vpn) begin tlb_d_hit = 1; data_ppn=tlb_ppn[0]; end
-	//	if (tlb_vpn_d[7] == ls_va[38:12]) begin bubble <= 1; tlb <= 0; pc <= pc - 4; end
-                //for (i=0;i<8;i=i+1) begin if (data_vpn == tlb_vpn[i]) begin tlb_d_hit = 1; data_ppn=tlb_ppn[i]; bubble <= 1; pc <= pc - 4; tlb <= 0; end end
-		//XXXABCDX*^^ABCDEFHXABCDEDHXAB|01^2^3^
 		tlb_d_hit = 0;
                 if (tlb_vld[0] && tlb_vpn[0] ==ls_va[38:12]) begin tlb_d_hit = 1; data_ppn=tlb_ppn[0]; bubble <= 1; pc <= pc - 4; tlb <= 0; end
                 if (tlb_vld[1] && tlb_vpn[1] ==ls_va[38:12]) begin tlb_d_hit = 1; data_ppn=tlb_ppn[1]; bubble <= 1; pc <= pc - 4; tlb <= 0; end
@@ -399,26 +395,26 @@ module riscv64(
 		tlb <= 1;
 
 
-        //    //  mmu_da  D-TLB miss Trap // load/store/atom
-//	    end else if (satp_mmu && !mmu_pc && !mmu_da && tlb_i_hit && !tlb_d_hit && (op == 7'b0000011 || op == 7'b0100011 || op == 7'b0101111) ) begin  
-//		mmu_da <= 1; // MMU_DA ON
-//		pc <= 28; // D-TLB refill Handler
-//	 	bubble <= 1'b1; // bubble
-//	        saved_user_pc <= pc - 4; // save pc EXE l/s/a
-//		for (i=0;i<10;i=i+1) begin sre[i]<= re[i]; end // save re
-//		re[9] <= ls_va; //save va to x1
-//                tlb_vpn_d[7] == ls_va[38:12];
-//		//if (op == 7'b0000011) re[9] <= rs1 + w_imm_i;
-//		//if (op == 7'b0100011) re[9] <= rs1 + w_imm_s;
-//		//if (op == 7'b0101111) re[9] <= rs1;
-//		Csrs[mstatus][MPIE] <= Csrs[mstatus][MIE]; // disable interrupt during shadow mmu walking
-//		Csrs[mstatus][MIE] <= 0;
-	    end else if (mmu_da && ir == 32'b00110000001000000000000001110011) begin // hiject mret 
-		pc <= saved_user_pc; // recover from shadow when see Mret
-		bubble <= 1; // bubble
-		for (i=0;i<10;i=i+1) begin re[i]<= sre[i]; end // recover usr re
-		mmu_da <= 0; // MMU_DA OFF
-		//Csrs[mstatus][MIE] <= Csrs[mstatus][MPIE]; // set back interrupt status
+//        //    //  mmu_da  D-TLB miss Trap // load/store/atom
+////	    end else if (satp_mmu && !mmu_pc && !mmu_da && tlb_i_hit && !tlb_d_hit && (op == 7'b0000011 || op == 7'b0100011 || op == 7'b0101111) ) begin  
+////		mmu_da <= 1; // MMU_DA ON
+////		pc <= 28; // D-TLB refill Handler
+////	 	bubble <= 1'b1; // bubble
+////	        saved_user_pc <= pc - 4; // save pc EXE l/s/a
+////		for (i=0;i<10;i=i+1) begin sre[i]<= re[i]; end // save re
+////		re[9] <= ls_va; //save va to x1
+////                tlb_vpn_d[7] == ls_va[38:12];
+////		//if (op == 7'b0000011) re[9] <= rs1 + w_imm_i;
+////		//if (op == 7'b0100011) re[9] <= rs1 + w_imm_s;
+////		//if (op == 7'b0101111) re[9] <= rs1;
+////		Csrs[mstatus][MPIE] <= Csrs[mstatus][MIE]; // disable interrupt during shadow mmu walking
+////		Csrs[mstatus][MIE] <= 0;
+//	    end else if (mmu_da && ir == 32'b00110000001000000000000001110011) begin // hiject mret 
+//		pc <= saved_user_pc; // recover from shadow when see Mret
+//		bubble <= 1; // bubble
+//		for (i=0;i<10;i=i+1) begin re[i]<= sre[i]; end // recover usr re
+//		mmu_da <= 0; // MMU_DA OFF
+//		//Csrs[mstatus][MIE] <= Csrs[mstatus][MPIE]; // set back interrupt status
 		
             // Interrupt PLIC full (Platform-Level-Interrupt-Control)  MMIO
 	    end else if ((meip_interrupt || msip_interrupt) && Csrs[mstatus][MIE]==1) begin //mstatus[3] MIE
@@ -890,8 +886,8 @@ module riscv64(
                 endcase
 	    end
         end
-	re[0]<=0; 
-	sre[0]<=0;
+	re[0]<= 64'h0; 
+	sre[0]<= 64'h0;
     end
 
 endmodule
