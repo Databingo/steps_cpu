10f98252e6fda08aa2817f3cf1ef1915c07e88b7
youhao <youhaohao@haohaohao.com>
2025-12-29 19:57:29 +0800

    update

--- cpu_on_board/cpu_on_board.v
+++ cpu_on_board/cpu_on_board.v
@@ -383,7 +383,7 @@ assign DRAM_CKE = 1; // always enable
 		//            6: begin sdram_addr <= bus_address[22:1]+3; sdram_byte_en <= 2'b11; sdram_read_en <= 1; step <= 7; end  
 		//	    7: if (sdram_req_wait==0) begin sdram_read_en <= 0; bus_read_data[63:48] <= sdram_rddata[15:0]; bus_read_done <= 1; step <=0; end
 		//	endcase
-	            3'b011: begin // ld 
+	            3'b011: begin // ld  // add infinite count force done=1
 		        case(step)
 			    0: begin sdram_addr <= bus_address[22:1];   sdram_byte_en <= 2'b11; sdram_read_en <= 1; step <= 1; end 
 			    1: if (sdram_req_wait==0) begin sdram_read_en <= 0; bus_read_data<= {48'b0, sdram_rddata[15:0]};bus_read_done <= 0; step <=10; end
