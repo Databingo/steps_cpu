b93af99080a0f8778545e4fca189d0467039c24b
youhao <youhaohao@haohaohao.com>
2026-02-08 20:40:47 +0800

    update

--- cpu_on_board/riscv64.v
+++ cpu_on_board/riscv64.v
@@ -836,9 +836,9 @@ module riscv64(
 			        pc <= pc -4;
 			        bubble <= 1;
 			    end else begin
-			        if (mul_type == 3'b000) re[w_rd] <= mul_result[63:0]; // mul low 64 always positive for mul
-			        else if (mul_sign) re[w_rd] <= ~mul_result[127:64] + (mul_result[63:0]==0);
-				else re[w_rd] <= mul_result[127:64];
+			        //if (mul_type == 3'b000) re[w_rd] <= mul_result[63:0]; // mul low 64 always positive for mul
+			        //else if (mul_sign) re[w_rd] <= ~mul_result[127:64] + (mul_result[63:0]==0);
+				//else re[w_rd] <= mul_result[127:64];
 			        mul_step <= 0;
 			    end
 			end
