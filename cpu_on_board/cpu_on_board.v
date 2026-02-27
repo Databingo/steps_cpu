f29193d00cb1c1b1d51e045e2a1fdddf479da42a
youhao <youhaohao@haohaohao.com>
2026-02-27 15:59:31 +0800

    update

--- cpu_on_board/cpu_on_board.v
+++ cpu_on_board/cpu_on_board.v
@@ -562,7 +562,7 @@ end
 
     // -- SD Card --
     wire [11:0] cid = (bus_address-`Sdc_base);
-    reg [11:0] cid;
+    //reg [11:0] cid;
     //reg [7:0] sd_cache [0:511];
     (* ram_style = "block" *) reg [7:0] sd_cache [0:511];
     //(* ram_style = "logic" *) reg [7:0] sd_cache [0:511];
