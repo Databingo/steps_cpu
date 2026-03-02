635bd6ea8e9e809b3a5e711f401b43e60352d454
youhao <youhaohao@haohaohao.com>
2026-03-02 19:07:04 +0800

    update

--- cpu_on_board/cpu_on_board.v
+++ cpu_on_board/cpu_on_board.v
@@ -612,7 +612,7 @@ end
     //reg [5:0] clkdiv = 0; // for 10M  10MHz/64 = 156.25KHz
     //reg [4:0] clkdiv = 0; // for 10M  10MHz/32 = 300KHz
     //reg [1:0] clkdiv = 0; // for 5M  5MHz/4 = 625Khz (sd SPI 100-400Khz)
-    reg [2:0] clkdiv = 0; // for 5M  5MHz/8 = 300Khz (sd SPI 100-400Khz)
+    reg [2:0] clkdiv = 0; // for 5M  5MHz/8 = 625Khz (sd SPI 100-400Khz)
     always @(posedge clock_slow or negedge KEY0) begin
     //always @(posedge CLOCK_50 or negedge KEY0) begin
         if (!KEY0) clkdiv <= 0;
