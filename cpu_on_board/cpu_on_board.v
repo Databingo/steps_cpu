70a2aa53693826be0b90dfa44f2846e051f9d760
youhao <youhaohao@haohaohao.com>
2026-03-04 09:59:50 +0800

    update

--- cpu_on_board/cpu_on_board.v
+++ cpu_on_board/cpu_on_board.v
@@ -603,7 +603,7 @@ end
 	    if (bus_write_enable && Sdc_read_selected) begin sd_cache_available <= 0; sd_rd_start <= 1;byte_index  <= 0; end //sd_rd_start <= 1; end
 	    //if (do_read && sd_status !=6) begin 
 	    if (byte_index == 512) begin 
-	        //sd_rd_start <= 0;
+	        sd_rd_start <= 0;
 	        byte_index <= 0;
 	        do_read <=0;
 	        sd_cache_available <= 1;
