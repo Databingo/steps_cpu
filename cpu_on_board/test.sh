42327d5eee8cc5d74d37d8dfbffa1ceb2a4995cd
youhao <youhaohao@haohaohao.com>
2026-03-06 18:11:57 +0800

    update

--- cpu_on_board/test.sh
+++ cpu_on_board/test.sh
@@ -34,14 +34,14 @@ cat bin.txt > rom.mif  # write ROM program
 #go run ../rvas.go csr.s # 
 #go run ../rvas.go SD.s # 
 #go run ../rvas.go SD2.s # 
-#go run ../rvas.go SD3.s # 
+go run ../rvas.go SD3.s # 
 #go run ../rvas.go sdram.s
 #go run ../rvas.go shadow.s
 #go run ../rvas.go mopensbi.s
 #go run ../rvas.go TLB.s
 #go run ../rvas.go PLIC.s # sdram write/read ok
 #go run ../rvas.go fake_opensbi.s
-go run ../rvas.go copy_sd_to_sdram.s
+#go run ../rvas.go copy_sd_to_sdram.s
 
 
 
