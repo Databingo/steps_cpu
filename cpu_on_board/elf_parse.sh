3490dbf2b0ead4a7bafbc9902bf22c1a3cd191db
youhao <youhaohao@haohaohao.com>
2026-03-08 16:41:03 +0800

    update

--- cpu_on_board/elf_parse.sh
+++ cpu_on_board/elf_parse.sh
@@ -2,7 +2,7 @@ bash test.sh
 file combined.o
 riscv64-unknown-elf-ld -o final.elf combined.o  -Ttext 0x0 -Tdata 0x1000 
 riscv64-unknown-elf-readelf -h final.elf
-
-
 riscv64-unknown-elf-objcopy -O binary final.elf final.bin
-#riscv64-unknown-elf-objdump -d final.elf
+#riscv64-unknown-elf-readelf -S final.elf
+riscv64-unknown-elf-objdump -b binary -m riscv -D final.bin
+
