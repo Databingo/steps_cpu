cd346aa7da45819cf977298b038a95a190ab6088
youhao <youhaohao@haohaohao.com>
2026-03-10 09:24:37 +0800

    update

--- cpu_on_board/elf_parse.sh
+++ cpu_on_board/elf_parse.sh
@@ -1,8 +1,8 @@
 bash test.sh
 file combined.o
-riscv64-unknown-elf-ld -o final.elf combined.o  -Ttext 0x0 -Tdata 0x1000 
+riscv64-unknown-elf-ld -o final.elf combined.o  -Ttext 0x0 -Tdata 0x1000  # linker
 riscv64-unknown-elf-readelf -h final.elf
-riscv64-unknown-elf-objcopy -O binary final.elf final.bin
+riscv64-unknown-elf-objcopy -O binary final.elf final.bin  # to binary
 #riscv64-unknown-elf-readelf -S final.elf
 riscv64-unknown-elf-objdump -b binary -m riscv -D final.bin
 
