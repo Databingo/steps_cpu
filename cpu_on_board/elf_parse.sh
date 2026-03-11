094900274f3384b5b46172a36476a4d576cf333b
youhao <youhaohao@haohaohao.com>
2026-03-11 14:31:58 +0800

    update

--- cpu_on_board/elf_parse.sh
+++ cpu_on_board/elf_parse.sh
@@ -21,14 +21,14 @@ riscv64-unknown-elf-readelf --all  linked.elf
 
 echo "---check linked.elf  --- "
 riscv64-unknown-elf-objdump -d linked.elf
-exit
+riscv64-unknown-elf-objdump -s -j .data linked.elf
 
 
-riscv64-unknown-elf-objcopy -O binary final.elf final.bin  # to binary
-#riscv64-unknown-elf-readelf -S final.elf
+riscv64-unknown-elf-objcopy -O binary linked.elf final.bin  # to binary
+hexdump -v -e '1/4 "%08x" "\n"' final.bin | perl -ne 'print unpack("B32", pack("N", hex($_))) . "\n"' > bin.txt 
+
 riscv64-unknown-elf-objdump -b binary -m riscv -D final.bin
-riscv64-unknown-elf-objdump -s -j .data final.elf
 
 
-hexdump -v -e '1/4 "%08x" "\n"' final.bin | perl -ne 'print unpack("B32", pack("N", hex($_))) . "\n"' > bin.txt
+
 
