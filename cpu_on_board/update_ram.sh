1fce9337f4b9010225c763d88bed8b71b17eb7b6
youhao <youhaohao@haohaohao.com>
2026-03-13 22:57:14 +0800

    update

--- cpu_on_board/update_ram.sh
+++ cpu_on_board/update_ram.sh
@@ -1,41 +1,28 @@
 #!/bin/bash
 
-# For example, if your project is "cpu_on_board.qpf", the name is "cpu_on_board".
 PROJECT_NAME="cpu_on_board"
 SOF_FILE="output_files/${PROJECT_NAME}.sof"
 
 # --- Stop on any error ---
 set -e
 
-# --- Compilation Flow ---
-
-echo "========================================="
-echo "Starting Quartus II Compilation Flow..."
-echo "Project: $PROJECT_NAME"
-echo "========================================="
-echo ""
-
+# Create roam.mif base on rom.mif and ram.mif
 bash roam.sh
 
-## update mif
+# Update mif
 quartus_cdb --read_settings_files=on --update_mif ${PROJECT_NAME} -c ${PROJECT_NAME}
-## Step 3: Assembler (quartus_asm) to SoF
-#echo "[3/3] Running Assembler..."
-#quartus_asm --read_settings_files=off --write_settings_files=off ${PROJECT_NAME} -c ${PROJECT_NAME}
-#echo "Assembler complete."
-#echo ""
 rm -f output_files/${PROJECT_NAME}.sof
 
+# Assembly
 quartus_asm --read_settings_files=on --write_settings_files=off ${PROJECT_NAME} -c ${PROJECT_NAME}
 
-# --- Stop on any error ---
+# Burn
 set -e
-
 quartus_pgm -m JTAG -c 1 --operation="P;${SOF_FILE}"
 
 echo ""
 echo "========================================="
 echo "Programming Update Successful!"
 echo "========================================="
-
+ 
 bash monitor.sh
