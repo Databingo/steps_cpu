e6c8c50f6981afbf221a4b0ce638e088e7c36307
youhao <youhaohao@haohaohao.com>
2026-03-13 21:35:13 +0800

    update

--- cpu_on_board/copy_sd_to_sdram3.s
+++ cpu_on_board/copy_sd_to_sdram3.s
@@ -211,7 +211,7 @@ stop_fun_print:
 msg:
     .string "Hello"
 sbi:
-    .string "I'm test Opensbi add update ram read=on"
+    .string "I'm test Opensbi add update ram read=on2"
 wait_sd_ready:
     .string "wait_sd_ready:"
 read_sd_sector:
