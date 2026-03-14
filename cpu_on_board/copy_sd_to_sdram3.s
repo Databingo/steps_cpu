830464b5cdf19626f7e92817e96878c347dac167
youhao <youhaohao@haohaohao.com>
2026-03-14 11:26:44 +0800

    update

--- cpu_on_board/copy_sd_to_sdram3.s
+++ cpu_on_board/copy_sd_to_sdram3.s
@@ -39,56 +39,7 @@ _start:
     la a1, sbi 
     call fun_print_string
 
-    
-
-
-
-# fake_opensbi  ------------------
-    lui t0, 0x2
-    addi t0, t0, 4       # UART = 0x2004
-    
-    # Print one byte
-    li t1, 0x58          # 'X'
-    sb t1, 0(t0)         # test
-    
-    # Load 4 byte
-    li t1, 0x49    # I
-    sb t1, 0(t0)    
-    li t1, 0x20    # space
-    sb t1, 0(t0)   
-    li t1, 0x61    # a
-    sb t1, 0(t0)   
-    li t1, 0x6d    # m
-    sb t1, 0(t0)   
-    li t1, 0x20    # space
-    sb t1, 0(t0)   
-    li t1, 0x6f    # o
-    sb t1, 0(t0)   
-    li t1, 0x70    # p
-    sb t1, 0(t0)   
-    li t1, 0x65    # e
-    sb t1, 0(t0)   
-    li t1, 0x6e    # n
-    sb t1, 0(t0)   
-    li t1, 0x73    # s
-    sb t1, 0(t0)   
-    li t1, 0x62    # b
-    sb t1, 0(t0)   
-    li t1, 0x69    # i
-    sb t1, 0(t0)   
-
-
 # ---------------------- SD card -------------------
-
-# UART base (for print_char)
-lui t0, 0x2
-addi t0, t0, 4      # t0 = 0x2004
-
-# SD controller base
-lui s1, 0x3         # a1 = 0x3000 base
-
-li t1, 65        # A
-sb t1, 0(s0)     # print
 
 # -- Read Boot Sector 0 -- 
 la a1, read_sd_sector
@@ -185,7 +136,7 @@ lw t2, 0x228(a1)    # t2 0x3228 cache_avaible
 beq t2, x0, wait_cache
 
 li t1, 70        # F
-sw t1, 0(t0)     # print
+sw t1, 0(s0)     # print
 ret
 
 
