860a18656ee06c127dde3122e9967813bb30754d
youhao <youhaohao@haohaohao.com>
2026-03-14 16:05:39 +0800

    update

--- cpu_on_board/bash.sh
+++ cpu_on_board/bash.sh
@@ -1,4 +1,4 @@
-while true; do read -p "CAUTION!\
-    Recompile 40 minutes! Not update 10 seconds! Enter to continue: "; 
+#while true; do read -p "CAUTION! Recompile 40 minutes! Not update 10 seconds! Enter to continue: "; 
+read -p "CAUTION! 
+Recompile 40 minutes! Not update 10 seconds! Enter to continue: "
 bash compile.sh && bash burn.sh && bash monitor.sh;
-done
