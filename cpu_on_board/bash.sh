a1eb78079b54ded3ac62ff73174f0ccb96179914
youhao <youhaohao@haohaohao.com>
2026-03-14 15:50:10 +0800

    update

--- cpu_on_board/bash.sh
+++ cpu_on_board/bash.sh
@@ -1,1 +1,4 @@
-bash compile.sh && bash burn.sh && bash monitor.sh
+
+while true: do read -p "Recompile 40 minutes! Not update 10 seconds!:"; 
+bash compile.sh && bash burn.sh && bash monitor.sh;
+done
