57a7b86de1f72ef9f2a9bc18f5d2b1e1fb22007a
youhao <youhaohao@haohaohao.com>
2026-03-14 10:34:58 +0800

    update

--- cpu_on_board/copy_sd_to_sdram3.s
+++ cpu_on_board/copy_sd_to_sdram3.s
@@ -87,12 +87,13 @@ lui a1, 0x3         # a1 = 0x3000 base
 
 li t1, 65        # A
 sb t1, 0(t0)     # print
-# -- Wait SD ready
-sd_ready:
-lw a2, 0x220(a1)    # a2 0x3220 ready
-li t1, 0x60        # `
-sb t1, 0(t0)     # print
-beq a2, x0, sd_ready
+
+## -- Wait SD ready
+#sd_ready:
+#lw a2, 0x220(a1)    # a2 0x3220 ready
+#li t1, 0x60        # `
+#sb t1, 0(t0)     # print
+#beq a2, x0, sd_ready
 
 # -- Read Boot Sector 0 -- 
 li a2, 0
