43acd54fdcc19b91441a6d427799ad1546e3ba1f
youhao <youhaohao@haohaohao.com>
2026-03-15 22:52:08 +0800

    update

--- cpu_on_board/copy_sd_to_sdram4.s
+++ cpu_on_board/copy_sd_to_sdram4.s
@@ -82,20 +82,21 @@ call putchar
 #mv a2, t2    # a2 = reserved_sectors offset 0x0e-0x0f 2 bytes (including root sector 0)
 
 
-#lbu a0, 0x0f(s1)
-#call print_hex_b
-#lbu a0, 0x0e(s1)
-#call print_hex_b
-#
-#li a0, 126       # ~
-#call putchar
-#
-#la t0, reserved_sec
-#lw a0, 0(t0)
-#call print_hex_b
+lbu a0, 0x0f(s1)
+call print_hex_b
+lbu a0, 0x0e(s1)
+call print_hex_b
+
+li a0, 126       # ~
+call putchar
+
+la t0, reserved_sec
+lw a0, 0(t0)
+call print_hex_b
 
 # root_dir_sector_start = reserved_sectors + (num_fats * sectors_per_fat16)
 
+# reserved_sectors offset 0x0e-0x0f 2 bytes (including root sector 0)
 li t0, "resSec:" # 7 char left on for null
 addi sp, sp, -8
 sd t0, 0(sp)
