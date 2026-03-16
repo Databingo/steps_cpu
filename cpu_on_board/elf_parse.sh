0d33d81a06b9a900067ae2bfc8a683b167eea1bd
youhao <youhaohao@haohaohao.com>
2026-03-16 23:34:47 +0800

    update

--- cpu_on_board/elf_parse.sh
+++ cpu_on_board/elf_parse.sh
@@ -9,8 +9,7 @@ riscv64-unknown-elf-objdump  -r  relocatable.elf
 echo "---read relocatable.elf relacatbal elf --- "
 riscv64-unknown-elf-readelf --all  relocatable.elf
  
-riscv64-unknown-elf-ld -o linked.elf   relocatable.elf  -Ttext 0x0 -Tdata 0x800  # linker ram totally 0x1000KB
-#riscv64-unknown-elf-ld -o final.elf combined.o  -Ttext 0x0 -Tdata 0x1000  # linker
+#riscv64-unknown-elf-ld -o linked.elf   relocatable.elf  -Ttext 0x0 -Tdata 0x800  # linker ram totally 0x1000KB
 #riscv64-unknown-elf-readelf -h  linked_caled.elf
 #riscv64-unknown-elf-readelf -S  linked_caled.elf
 echo "---read linked.elf  --- "
@@ -22,7 +21,7 @@ riscv64-unknown-elf-objdump -s -j .data linked.elf
 
 echo "---extract raw instructions --- "
 riscv64-unknown-elf-objcopy -O binary linked.elf final.bin  # to binary
-hexdump -v -e '1/4 "%08x" "\n"' final.bin | perl -ne 'print unpack("B32", pack("N", hex($_))) . "\n"' > bin.txt 
+#hexdump -v -e '1/4 "%08x" "\n"' final.bin | perl -ne 'print unpack("B32", pack("N", hex($_))) . "\n"' > bin.txt 
 
 #riscv64-unknown-elf-objdump -b binary -m riscv -D final.bin
 
