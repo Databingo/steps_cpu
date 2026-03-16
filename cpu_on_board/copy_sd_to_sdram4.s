c0abf377c4796ce89e67d7acbde2819acdb25dcc
youhao <youhaohao@haohaohao.com>
2026-03-16 15:37:55 +0800

    update

--- cpu_on_board/copy_sd_to_sdram4.s
+++ cpu_on_board/copy_sd_to_sdram4.s
@@ -178,14 +178,27 @@ call print_hex_b
 li t1, 124       # |
 sb t1, 0(s11)     # print
 
-addi a0, x0, 8
+addi a0, x0, 7
 call print_reg
 li a0, 43       # +
 call putchar
 
 
 mv a0, t1
+call print_reg
+li a0, 43       # +
+call putchar
+
+mv a0, t2
+call print_reg
+li a0, 43       # +
+call putchar
+
 mul a0, t1, t2
+call print_reg
+li a0, 43       # +
+call putchar
+
 add a0, a0, t0
 call print_reg
 
