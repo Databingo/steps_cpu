7c0fe7e9c4c6c3f70ffe326eeee9c2284e084deb
youhao <youhaohao@haohaohao.com>
2026-03-18 15:34:47 +0800

    update

--- cpu_on_board/riscv64.v
+++ cpu_on_board/riscv64.v
@@ -365,30 +365,22 @@ module riscv64(
     reg [127:0] div_rem;   // remainder|quotient
     reg [63:0]  div_a;    // be divided
     reg [63:0]  div_b;    // divisor
-    //reg [63:0]  div_b_abs;    // divisor
     reg         div_active; // 1computing, 0idle
     reg         div_done;   // handshake 1result ready
     reg         div_enable; // handshake 1start request
-    //reg         div_sign_quotient; // sige of quotient
-    //reg         div_sign_reminder; // sige of remainder
     reg         div_is_rem; // 1rem, 0div
     reg [63:0]  div_result_out; // final output buffer
     reg [4:0]   div_rd; 
-    reg         div_op_signed_latched;
+    reg         div_op_signed;
 
-    
-    // signal 100div/w 101divu 110rem/w 111remu
-    wire div_op_signed = !ir[12];  // func3[0] == 0 is signed
-    wire div_op_is_rem = ir[13];   // func3[1] == 1 is rem
-
-    wire a_is_neg = div_op_signed_latched && div_a[63];
-    wire b_is_neg = div_op_signed_latched && div_b[63];
+    wire a_is_neg = div_op_signed && div_a[63];
+    wire b_is_neg = div_op_signed && div_b[63];
     wire [63:0] div_abs_a = a_is_neg ? (~div_a + 64'd1):div_a;
     wire [63:0] div_abs_b = b_is_neg ? (~div_b + 64'd1):div_b;
-    wire out_sign_quo = div_op_signed_latched && (div_a[63] ^ div_b[63]);
+    wire out_sign_quo = div_op_signed && (div_a[63] ^ div_b[63]);
     wire out_sign = div_is_rem ? a_is_neg : out_sign_quo;
     wire [63:0] raw_out = div_is_rem ? div_rem[127:64] : div_rem[63:0];
-    wire [63:0] final_out = out_sign ? (~raw_out+64'd1):raw_out;
+    wire [63:0] final_out = out_sign ? (~raw_out+64'd1): raw_out;
 
     always @(posedge clk or negedge reset) begin
         if (!reset) begin
@@ -400,7 +392,6 @@ module riscv64(
         	// start phase
         	div_active <= 1;
         	div_cnt <= 0;
-        	//div_is_rem <= div_op_is_rem;
         	// handle corner case
         	if (div_b == 0) begin
         	    // divide by zero
@@ -408,28 +399,22 @@ module riscv64(
         	    div_active <= 0;
         	    div_done <= 1; // finish immediately
         	end
-        	else if (div_op_signed_latched && div_a == 64'h8000000000000000 && div_b == ~64'd0) begin // ??
+        	else if (div_op_signed && div_a == 64'h8000000000000000 && div_b == ~64'd0) begin // ??
         	    // signed overflow
         	    div_result_out <= div_is_rem ? 64'd0 : div_a;
         	    div_active <= 0;
         	    div_done <= 1; // finish immediately
         	end
         	else begin 
-        	//    // mormal division setup
-        	//    // 1. determine signs
-        	//    div_sign_reminder <= div_op_signed_latched ?  div_a[63] :0;
-        	//    div_sign_quotient <= div_op_signed_latched ? (div_a[63] & div_b[63]) :0;
-        	//    // 2. load absoulte values
-        	    //div_rem <= {64'd0, (div_op_signed_latched && div_a[63]) ? -div_a : div_a};
         	    div_rem <= {64'd0, div_abs_a};
-        	//    div_b <= (div_op_signed_latched &&  div_b[63]) ? -div_b : div_b;
-        	    //div_b_abs <= div_abs_b;
         	end
             end else if (div_active) begin
         	// compute phase (64 cycles)
         	if (div_cnt < 64) begin
-        	    if (div_rem[126:63] >= div_abs_b) begin
-        	        div_rem <= {div_rem[126:63] - div_abs_b, div_rem[62:0], 1'b1};
+        	    //if (div_rem[126:63] >= div_abs_b) begin
+        	    if (div_rem[127:64] >= div_abs_b) begin
+        	        //div_rem <= {div_rem[126:63] - div_abs_b, div_rem[62:0], 1'b1};
+        	        div_rem <= {div_rem[127:64] - div_abs_b, div_rem[62:0], 1'b1};
         	    end else begin
         	        div_rem <= {div_rem[126:0], 1'b0};
         	    end
@@ -438,8 +423,6 @@ module riscv64(
         	    // finish phase
         	    div_active <= 0;
         	    div_done   <= 1;
-        	    //if (div_is_rem) div_result_out <= div_sign_reminder ? -div_rem[127:64] : div_rem[127:64];
-        	    //else div_result_out <= div_sign_quotient ? -div_rem[63:0] : div_rem[63:0];
         	    div_result_out <= final_out;
         	end
             end else if (!div_enable) div_done <= 0; // reset handshake
@@ -1090,10 +1073,9 @@ module riscv64(
 			    if (!div_enable) begin
                                 div_a <= rs1;    // be divided
                                 div_b <= rs2;    // divisor
-                                //div_a <= (div_op_signed &&  rs1[63]) ? -rs1:rs1;
-                                //div_b <= (div_op_signed &&  rs2[63]) ? -rs2:rs2;
-                                div_op_signed_latched <= div_op_signed;
-                                div_is_rem <= div_op_is_rem; // 1rem, 0div
+                                // 100div/w 101divu 110rem/w 111remu
+                                div_op_signed <= !ir[12];  // func3[0] == 0 is signed
+                                div_is_rem <= ir[13];   // func3[1] == 1 is rem
                                 div_rd <= w_rd; 
 			    end
 
