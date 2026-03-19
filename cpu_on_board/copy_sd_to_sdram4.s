70b4d7e0a3388629c41f4b984d160489741cb7d0
youhao <youhaohao@haohaohao.com>
2026-03-19 20:57:21 +0800

    update

--- cpu_on_board/copy_sd_to_sdram4.s
+++ cpu_on_board/copy_sd_to_sdram4.s
@@ -337,116 +337,108 @@ j entry_loop
 #| `0x1C` | 4    | **File size (bytes)**             | File length                  | 4096         |
 
 read_file:
-## file size at 0x1C-0x1D-0x1E-0x1F 4 bytes
-li a0, "\nFSize:"
-call print7
-lwu a0, 0x1c(t3)
-la a1, file_size
-sw a0, 0(a1)
-lwu a0, 0(a1)
-call print_reg
-
-# file_first_cluster at 0x1A-0x1B 2 bytes
-li a0, "\nF0cls:"
-call print7
-lhu a0, 0x1a(t3)
-la a1, file_first_cluster
-sh a0, 0(a1)
-lhu a0, 0(a1)
-call print_reg
-
-# root_dir_sectors = (RootEntryCount * 32 + BytesPerSector -1 )/ BytesPerSector  ceiling division
-li a0, "\nrDseS:"
-call print7
-
-la a1, root_ent_cnt
-lwu t0, 0(a1)
-li t1, 32
-mul t2, t0, t1
-
-la a1, byte_per_sec
-lwu t0, 0(a1)
-add t3, t2, t0
-addi t3, t3, -1
-div a0, t3, t0
-
-la a1, root_dir_sectors 
-sw a0, 0(a1)
-lw a0, 0(a1)
-call print_reg
-
-#data_start_sec # FirstDataSector = root_dir_sector_start + root_dir_sectors 
-li a0, "\nD0sec:"
-call print7
-la a1, root_dir_sector_start
-lw t0, 0(a1)
-la a1, root_dir_sectors 
-lw t1, 0(a1)
-add t2, t0, t1
-la a1, data_start_sec  
-sw t2, 0(a1)
-lw a0, 0(a1)
-call print_reg
-
-# FirstSectorOfCluster(N)=FirstDataSector + (N - 2) * SectorsPerCluster
-li a0, "\nF0sec:"
-call print7
-
-la a1, file_first_cluster # N
-lw t0, 0(a1)
-addi t0, t0, -2
-la a1, sec_per_clus
-lw t1, 0(a1)
-mul t2, t0, t1
-la a1, data_start_sec
-lw t3, 0(a1)
-add a0, t3, t2
-la a1, file_start_sector
-sw a0, 0(a1)
-call print_reg
-
-
-# file_sectors = file_size + 511 / 512
-li a0, "\nFseS:"
-call print7
-la a1, file_size
-lw t0, 0(a1)
-addi t0, t0, 511
-li t1, 512
-div a0, t0, t1
-la a1, file_sectors 
-sw a0, 0(a1)
-lw a0, 0(a1)
-call print_reg
-
-
-li a0, "\nFileB:"
-call print7
-
-
-la a1, file_sectors
-lw t0, 0(a1)
-la a1,  file_start_sector
-lw t1, 0(a1)
-add t2, t1, t0
-
-print_sector_loop:
-mv a0, t1
-call sd_read_sector  # use a0 as sector no.
-call print_sector
-addi t1, t1, 1
-blt t1, t2, print_sector_loop
-
-
-
-
-
-
-
-
-
-
-
+### file size at 0x1C-0x1D-0x1E-0x1F 4 bytes
+#li a0, "\nFSize:"
+#call print7
+#lwu a0, 0x1c(t3)
+#la a1, file_size
+#sw a0, 0(a1)
+#lwu a0, 0(a1)
+#call print_reg
+#
+## file_first_cluster at 0x1A-0x1B 2 bytes
+#li a0, "\nF0cls:"
+#call print7
+#lhu a0, 0x1a(t3)
+#la a1, file_first_cluster
+#sh a0, 0(a1)
+#lhu a0, 0(a1)
+#call print_reg
+#
+## root_dir_sectors = (RootEntryCount * 32 + BytesPerSector -1 )/ BytesPerSector  ceiling division
+#li a0, "\nrDseS:"
+#call print7
+#
+#la a1, root_ent_cnt
+#lwu t0, 0(a1)
+#li t1, 32
+#mul t2, t0, t1
+#
+#la a1, byte_per_sec
+#lwu t0, 0(a1)
+#add t3, t2, t0
+#addi t3, t3, -1
+#div a0, t3, t0
+#
+#la a1, root_dir_sectors 
+#sw a0, 0(a1)
+#lw a0, 0(a1)
+#call print_reg
+#
+##data_start_sec # FirstDataSector = root_dir_sector_start + root_dir_sectors 
+#li a0, "\nD0sec:"
+#call print7
+#la a1, root_dir_sector_start
+#lw t0, 0(a1)
+#la a1, root_dir_sectors 
+#lw t1, 0(a1)
+#add t2, t0, t1
+#la a1, data_start_sec  
+#sw t2, 0(a1)
+#lw a0, 0(a1)
+#call print_reg
+#
+## FirstSectorOfCluster(N)=FirstDataSector + (N - 2) * SectorsPerCluster
+#li a0, "\nF0sec:"
+#call print7
+#
+#la a1, file_first_cluster # N
+#lw t0, 0(a1)
+#addi t0, t0, -2
+#la a1, sec_per_clus
+#lw t1, 0(a1)
+#mul t2, t0, t1
+#la a1, data_start_sec
+#lw t3, 0(a1)
+#add a0, t3, t2
+#la a1, file_start_sector
+#sw a0, 0(a1)
+#call print_reg
+#
+#
+## file_sectors = file_size + 511 / 512
+#li a0, "\nFseS:"
+#call print7
+#la a1, file_size
+#lw t0, 0(a1)
+#addi t0, t0, 511
+#li t1, 512
+#div a0, t0, t1
+#la a1, file_sectors 
+#sw a0, 0(a1)
+#lw a0, 0(a1)
+#call print_reg
+#
+#
+#li a0, "\nFileB:"
+#call print7
+#
+#
+#la a1, file_sectors
+#lw t0, 0(a1)
+#la a1,  file_start_sector
+#lw t1, 0(a1)
+#add t2, t1, t0
+#
+#print_sector_loop:
+#mv a0, t1
+#call sd_read_sector  # use a0 as sector no.
+#call print_sector
+#addi t1, t1, 1
+#blt t1, t2, print_sector_loop
+#
+#
+#
 end: 
 j end
 
