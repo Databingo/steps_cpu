27602776c84dd00da85bbc3a06a211fbecb6c83e
youhao <youhaohao@haohaohao.com>
2026-03-20 11:23:16 +0800

    cp_sd_2_sdram_print_ok

--- cpu_on_board/copy_sd_to_sdram4.s
+++ cpu_on_board/copy_sd_to_sdram4.s
@@ -426,21 +426,21 @@ blt t1, t2, copy_sector_loop
 #sb t2, 0(s11)         # Should print 'X'
 
 
-    lui t0, 0x2
-    addi t0, t0, 4       # UART = 0x2004
-    
-    lui s0, 0x10000      # SDRAM = 0x10000000
-    
-    # Write one byte
-    li t1, 0x58          # 'X'
-    call wait_uart
-    sb t1, 0(s0)         # test sdram sb/sh
-    
-    # Read it back
-    lbu t2, 0(s0)         # test sdram lbu
-    
-    # Print it
-    sb t2, 0(t0)         # Should print 'X'
+#    lui t0, 0x2
+#    addi t0, t0, 4       # UART = 0x2004
+#    
+#    lui s0, 0x10000      # SDRAM = 0x10000000
+#    
+#    # Write one byte
+#    li t1, 0x58          # 'X'
+#    call wait_uart
+#    sb t1, 0(s0)         # test sdram sb/sh
+#    
+#    # Read it back
+#    lbu t2, 0(s0)         # test sdram lbu
+#    
+#    # Print it
+#    sb t2, 0(t0)         # Should print 'X'
 #    sb t2, 0(t0)         # Should print 'X'
 #    sh t2, 0(t0)         # Should print 'X'
 #    
