ab144775683e6acf7e1836f09f158b5cd1288d1f
youhao <youhaohao@haohaohao.com>
2026-03-23 20:54:46 +0800

    update

--- cpu_on_board/cpu_on_board.v
+++ cpu_on_board/cpu_on_board.v
@@ -320,7 +320,8 @@ assign DRAM_CKE = 1; // always enable
 	    uart_write_pulse <= 0;
 	    uart_read_pulse <= 0;
 	    uart_read_step <= 0;
-	    mtimecmp <=  64'h80000000;
+	    //mtimecmp <=  64'h80000000;
+	    mtimecmp <=  64'hffffffffffffffff;
 	    uart_irq_pre <= 0;
             //sd_rd_start <= 0;
 	end else begin
@@ -661,7 +662,7 @@ end
     end
 
     // Slow pulse clock for SD init (~100 kHz)
-    reg [8:0] clkdiv = 0;  // for 50M
+    reg [8:0] clkdiv = 0;  // for 50M  12.5M/512=97.6Khz
     //reg [7:0] clkdiv = 0;  // for 25M
     //reg [5:0] clkdiv = 0; // for 10M  10MHz/64 = 156.25KHz
     //reg [4:0] clkdiv = 0; // for 10M  10MHz/32 = 300KHz
