e35c37b0a7ae4602566d8677b006a6467119732c
youhao <youhaohao@haohaohao.com>
2026-03-24 21:47:56 +0800

    update

--- cpu_on_board/atom.s
+++ cpu_on_board/atom.s
@@ -161,6 +161,30 @@ call print_reg # old value
 lw a0, 0(s0)
 call print_reg # new value
 
+# atom test ------
+li a0, "\nAand.w"
+call print7
+
+li t0, 10 
+sw t0, 0(s0)
+
+li t0, 5
+amoand.w t1, t0, (s0)
+
+mv a0, t1
+call print_reg # old value
+
+lw a0, 0(s0)
+call print_reg # new value
+
+
+
+
+
+
+
+
+
 enda:
    j enda
 
