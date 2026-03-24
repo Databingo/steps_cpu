182d3f2b695463d5ff57dc7a88e45a5441a44f3f
youhao <youhaohao@haohaohao.com>
2026-03-24 21:51:42 +0800

    update

--- cpu_on_board/atom.s
+++ cpu_on_board/atom.s
@@ -178,6 +178,21 @@ lw a0, 0(s0)
 call print_reg # new value
 
 
+# atom test ------
+li a0, "\nAor.w"
+call print7
+
+li t0, 10 
+sw t0, 0(s0)
+
+li t0, 4
+amoand.w t1, t0, (s0)
+
+mv a0, t1
+call print_reg # old value
+
+lw a0, 0(s0)
+call print_reg # new value
 
 
 
