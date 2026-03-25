020ca7665cf9bb804588c61c9436eddc6131c762
youhao <youhaohao@haohaohao.com>
2026-03-25 15:55:14 +0800

    update

--- cpu_on_board/mini_sbi.s
+++ cpu_on_board/mini_sbi.s
@@ -2,79 +2,114 @@
 .globl _start
 
 _start:
-   # 1. setup m-mode trap handler (sbi interface)
-   la t0, sbi_trap_handler
+   la t0, m_trap_router      # 1. setup m-mode trap handler (sbi interface)
    csrw mtvec, t0
 
-   la t0, s_trap_handler # setup s-mode trap handler
+   la t0, s_trap_handler     # 2. setup s-mode trap handler
    csrw stvec, t0
 
 
-
-   # 2. unlock PMP (Physical Memory Protection) which prevent S-mode to touch hardware
+   # 2. Unlock PMP (Physical Memory Protection) which prevent S-mode to touch hardware
    li t0, 0xffffffffffffffff
    csrw pmpaddr0, t0
    li t0, 0x0f    # NAPOT mode, Read/Write/Execute permissions
    csrw pmpcfg0, t0
- 
+
+   # No delegation (let M-mode handler take care)
+   csrw medeleg, zero
+   # Delegate breakpoint to s-mode
+   #li t0, 0b1000
+   #csrw medeleg, t0
+
    # 3. prepare the jump to S-mode
-   #li t0, (1<<11) # set mstatus.MPP to 1, Bit 11 is MPP
    li t0, 0b100000000000 # set mstatus.MPP to 1, Bit 11 is MPP
    csrs mstatus, t0
    # set mepc to the address of our S-mode kernel
    la t0, s_mode_kernel
    csrw mepc, t0
-
-   # Look at mstatus.MPP but see S-mode, jump to mepc then transition to S-mode
+   
+   # 4. Drop to S-mode go to s_kernel
    mret
 
 
+m_trap_router:
+   csrr t0, mcause  # check type
+   li t1, 9  # ecall from S-mode
+   beq t0, t1, m_ecall_router
 
+   li t1, 3 # breakpoint
+   beq t0, t1, m_ebreak_handler
 
+   j m_done
 
-
-
-
-
-
-
-
-
-sbi_trap_handler:  # a7 Extension 1 putchar, 2 getchar, 0 settimer; a0 is the first argument a1..a5
+m_ecall_router:  # a7 Extension 1 putchar, 2 getchar, 0 settimer; a0 is the first argument a1..a5
    li t0, 1 # look a7 to see function is requested(sbi standard)
-   beq a7, t0, sbi_handler_putchar  # (SBI_CONSOLE_PUTCHAR ( a7 = 0x01)
+   beq a7, t0, m_handler_putchar  # (SBI_CONSOLE_PUTCHAR ( a7 = 0x01)
 
    li t0, 0
-   beq a7, t0, sbi_handler_timer
+   beq a7, t0, m_handler_timer
+
+   li t0, 0x10
+   beq a7, t0, m_handler_deleg
 
    li a0, -1  # unknow SBI_ERR_NOT_SUPPORTED = -1
-   j ecall_done
-  
-sbi_handler_putchar:
+   j m_done
+
+m_ebreak_handler:
+   li a0, "M"
+   li s11, 0x2004
+wait_uart_m:
+   lw t1, 0(s11)
+   blt t1, zero, wait_uart_m  # sifive uart negetive is full
+   sb a0, 0(s11)
+   li a0, 0     # return success
+   j m_done
+   
+m_handler_putchar:
    li s11, 0x2004
 wait_uart_sbi:
    lw t1, 0(s11)
    blt t1, zero, wait_uart_sbi  # sifive uart negetive is full
    sb a0, 0(s11)
    li a0, 0     # return success
-   j ecall_done
+   j m_done
 
-sbi_handler_timer:
+m_handler_timer:
    li t1, 0x2004000  # riscv use MMIP for mtimecmp
    sd a0, 0(t1)      # S-mode passes new time in a0
    li a0, 0          # Success return a0=0
-   j ecall_done
-   
+   j m_done
 
+m_handler_deleg:
+   li t0, 0b1000
+   csrw medeleg, t0 
+   li a0, 0
+   j m_done
 
-ecall_done: 
+m_done: 
    csrr t0, mepc
-   addi t0, t0, 4 # skip ecall instruction
+   addi t0, t0, 4 # skip ecall/ebreak instruction
    csrw mepc, t0
    mret
-   
+
+
+s_trap_handler:
+   li a0, "S"
+   li a7, 1
+   ecall
+   j s_done
+
+s_done: 
+   csrr t0, sepc
+   addi t0, t0, 4 # skip ecall/ebreak instruction
+   csrw sepc, t0
+   sret
+
+
 
 s_mode_kernel:
+   ebreak   # M
+
    li a0, "H"
    li a7, 1
    ecall
@@ -82,21 +117,12 @@ s_mode_kernel:
    li a0, "i"
    li a7, 1
    ecall
-
-   li t0, 0b1000  # delegate breakpoint to s-mode
-   csrw medeleg, t0
-   ebreak
-
-halt:
-   j halt
+ 
+   li a7, 0x10
+   ecall  # turn delegate
+   ebreak # S
 
 s_mode_done:
    j s_mode_done
 
-
-s_trap_handler:
-   li a0, "S"
-   li a7, 1
-   ecall
-   j halt
 
