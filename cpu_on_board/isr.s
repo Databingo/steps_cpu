3ca82feeecd74ebe237e775cab5121695fa89b97
youhao <youhaohao@haohaohao.com>
2026-03-27 18:16:21 +0800

    update

--- cpu_on_board/isr.s
+++ cpu_on_board/isr.s
@@ -41,82 +41,82 @@
 
 mmu:  # VA 63:39Sign|38:30Vpn[2]|29:21Vpn[1]|20:12Vpn[0]|11:0PageOffset  
    # 1. Get root table address from csr satp Supervisor Address Translation and Protection
-     csrr x5, satp   # satp 63:60Mode|59:44Asid(0forSimpleOS)|43:0PPNofRootTable
-     slli x5, x5, 20 # clear high mode+Asid Address Space Identifier
-     srli x5, x5, 8  # get level_2 ppn(27 bits) + 12 zero positon, point to start of Root Table
+     csrr x2, satp   # satp 63:60Mode|59:44Asid(0forSimpleOS)|43:0PPNofRootTable
+     slli x2, x2, 20 # clear high mode+Asid Address Space Identifier
+     srli x2, x2, 8  # get level_2 ppn(27 bits) + 12 zero positon, point to start of Root Table
 
    # 2. Level 2 walk
-     srli x6, x9, 30 # extract vpn[2] bit 38:30 the first 9 bits
-     andi x6, x6, 0x1ff # Mask 9 bits
-     slli x6, x6, 3  # Multiple by 8 (PTE size 8 bytes) Page Table Entry 64 bits
-     add  x5, x5, x6 # x5 = Address of L2 PTE
-     ld x7, 0(x5)    # Load L2 PTE from memory  PTE 63:54Reserved|53:10PPN|9:8RSW|XWRmark|0validBit1
+     srli x3, x9, 30 # extract vpn[2] bit 38:30 the first 9 bits
+     andi x3, x3, 0x1ff # Mask 9 bits
+     slli x3, x3, 3  # Multiple by 8 (PTE size 8 bytes) Page Table Entry 64 bits
+     add  x2, x2, x3 # x2 = Address of L2 PTE
+     ld x4, 0(x2)    # Load L2 PTE from memory  PTE 63:54Reserved|53:10PPN|9:8RSW|XWRmark|0validBit1
 
    # 3. Check Leaf
-     andi x6, x7, 0xE # bit 3:1 for X/W/R
-     bnez x6, FINISH_1GB  # If not zero, it's leaf. We get the address.
-     andi x6, x7, 1   # check PTE valid bit
-     beqz x6, FAULT
+     andi x3, x4, 0xE # bit 3:1 for X/W/R
+     bnez x3, FINISH_1GB  # If not zero, it's leaf. We get the address.
+     andi x3, x4, 1   # check PTE valid bit
+     beqz x3, FAULT
 
    # 4. Prepare for Level 1
-     srli x5, x7, 10 # Extract PPN from L2 PTE
-     slli x5, x5, 12 # x5 = Address of L1 Table
+     srli x2, x4, 10 # Extract PPN from L2 PTE
+     slli x2, x2, 12 # x2 = Address of L1 Table
 
    # 5. Level 1 Walk
-     srli x6, x9, 21 # Extract VPN[1] bit 29:21
-     andi x6, x6, 0x1ff # Mask 9 bits
-     slli x6, x6, 3  # Multiple by 8 (PTE size 8 bytes)
-     add  x5, x5, x6 # x5 = Address of L1 PTE
-     ld x7, 0(x5)    # Load L1 PTE from memory
+     srli x3, x9, 21 # Extract VPN[1] bit 29:21
+     andi x3, x3, 0x1ff # Mask 9 bits
+     slli x3, x3, 3  # Multiple by 8 (PTE size 8 bytes)
+     add  x2, x2, x3 # x2 = Address of L1 PTE
+     ld x4, 0(x2)    # Load L1 PTE from memory
 
    # 6. Check Leaf
-     andi x6, x7, 0xE # bit 3:1 for X/W/R
-     bnez x6, FINISH_2MB   # If not zero, it's leaf. We get the address.
-     andi x6, x7, 1   # check PTE valid bit
-     beqz x6, FAULT
+     andi x3, x4, 0xE # bit 3:1 for X/W/R
+     bnez x3, FINISH_2MB   # If not zero, it's leaf. We get the address.
+     andi x3, x4, 1   # check PTE valid bit
+     beqz x3, FAULT
 
    # 7. Prepare for Level 0
-     srli x5, x7, 10 # Extract PPN from L1 PTE
-     slli x5, x5, 12 # x5 = Address of L0 Table
+     srli x2, x4, 10 # Extract PPN from L1 PTE
+     slli x2, x2, 12 # x2 = Address of L0 Table
 
    # 8. Level 0 Walk
-     srli x6, x9, 12 # Extract VPN[0] bit 20:12
-     andi x6, x6, 0x1ff # Mask 9 bits
-     slli x6, x6, 3  # Multiple by 8 (PTE size 8 bytes)
-     add  x5, x5, x6 # x5 = Address of L0 PTE
-     ld x7, 0(x5)    # Load L0 PTE from memory
+     srli x3, x9, 12 # Extract VPN[0] bit 20:12
+     andi x3, x3, 0x1ff # Mask 9 bits
+     slli x3, x3, 3  # Multiple by 8 (PTE size 8 bytes)
+     add  x2, x2, x3 # x2 = Address of L0 PTE
+     ld x4, 0(x2)    # Load L0 PTE from memory
 
    # 9. Check valid
-     andi x6, x7, 1   # check PTE valid bit
-     beqz x6, FAULT
+     andi x3, x4, 1   # check PTE valid bit
+     beqz x3, FAULT
    # fall to 4KB finish
 
 FINISH_4KB:
-     srli x7, x7, 10 
-     slli x7, x7, 12 
+     srli x4, x4, 10 
+     slli x4, x4, 12 
      j WRITE_TLB
                                             
 FINISH_2MB:
-     srli x7, x7, 10 
-     slli x7, x7, 12 
-     li x6, 0x001ff000 # mask for VA[20:12]
-     and x6, x6, x9    
-     add x7, x7, x6   
+     srli x4, x4, 10 
+     slli x4, x4, 12 
+     li x3, 0x001ff000 # mask for VA[20:12]
+     and x3, x3, x9    
+     add x4, x4, x3   
      j WRITE_TLB
 
 FINISH_1GB:
-     srli x7, x7, 10  # get PPN from PTE(PTE's data struction?)  64:54Reserved 53:10PPN # 9:8RSW 7Dirty 6Accessed 5Global 4User 3Executable 2Write 1Readable 0Valid
-     slli x7, x7, 12  # PPN posint [38:12] in satp
-     li x6, 0x3ffff000 # mask for VA[29:12]
-     and x6, x6, x9    # extrac from VA
-     add x7, x7, x6    # add offset to PPN
+     srli x4, x4, 10  # get PPN from PTE(PTE's data struction?)  64:54Reserved 53:10PPN # 9:8RSW 7Dirty 6Accessed 5Global 4User 3Executable 2Write 1Readable 0Valid
+     slli x4, x4, 12  # PPN posint [38:12] in satp
+     li x3, 0x3ffff000 # mask for VA[29:12]
+     and x3, x3, x9    # extrac from VA
+     add x4, x4, x3    # add offset to PPN
      j WRITE_TLB
                  
 
 WRITE_TLB:
      # 9. Writ ppn back to hardware mmu trap
-     lui x8, 0x20000 # Magic TLB address
-     sd x7, 0(x8)
+     lui x2, 0x20000 # Magic TLB address
+     sd x4, 0(x2)
 
      lui x1, 0x2     
      addi x1, x1, 0x4              
