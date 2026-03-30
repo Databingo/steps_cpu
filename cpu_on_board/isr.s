a6d77e9230c69f7d51c735982b67f5685e2bb1a1
youhao <youhaohao@haohaohao.com>
2026-03-30 19:04:44 +0800

    update

--- cpu_on_board/isr.s
+++ cpu_on_board/isr.s
@@ -20,6 +20,7 @@ i_cache_refill:
      addi x3, x3, 0x4  # set print           
      addi x2, x0, 0x25
      sd x2, 0(x3)    #  print %
+  mv x1, x9 
      mret           
 
 mmu:  # VA 63:39Sign|38:30Vpn[2]|29:21Vpn[1]|20:12Vpn[0]|11:0PageOffset  
@@ -119,6 +120,7 @@ WRITE_TLB:
      sd x2, 0(x3)    #  print [
 
   mv a0, x5
+  mv x1, x9 
      mret
 
 
@@ -127,6 +129,7 @@ FAULT: # error trap?
      addi x3, x3, 0x4              
      addi x2, x0, 33
      sd x2, 0(x3)    #  print !
+  mv x1, x9 
      mret
      
 #Seems VA has 3 table number, satp has Root Table(vpn[2]) address via PPN(ppn+12 x7ace), the we can find PTE in table 2, and PTE has PPN, we can use table2PPN to find table 1 address plus vpn1 number to find PTE in table1, then we get table1 PPN for table0 address, and together with vpn0 to find PTE in talbe0, this is  the last ppa, by ppn + 12 bit of VA low.
