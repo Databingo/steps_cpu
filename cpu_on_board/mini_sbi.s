48a2e97f5467930f99f22bf29b73520a51cfac51
youhao <youhaohao@haohaohao.com>
2026-03-30 23:27:30 +0800

    update

--- cpu_on_board/mini_sbi.s
+++ cpu_on_board/mini_sbi.s
@@ -320,12 +320,12 @@ wait_uart:
     ret
 
 print7: # a0, 7 char left one for null
-    addi sp, sp, -8
-   #sd a0, 0(sp)
-    sd ra, 0(sp)
+    addi sp, sp, -16
+    sd a0, 0(sp)
+    sd ra, 8(sp)
     mv a0, sp
     call puts
-   #ld a0, 0(sp)
-    ld ra, 0(sp)
-    addi sp, sp, 8
+    ld a0, 0(sp)
+    ld ra, 8(sp)
+    addi sp, sp, 16
     ret
