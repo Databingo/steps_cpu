a007fa8dab3f0f3a97c23089714870a547377491
youhao <youhaohao@haohaohao.com>
2026-04-09 16:47:34 +0800

    update

--- cpu_on_board/isr.s
+++ cpu_on_board/isr.s
@@ -202,11 +202,15 @@ debug:
      csrr a0, mimpid # mimpid/pda
      call print_reg
 
-     li a0, "|mhartid:" 
-     call print7
-     csrr a0, mhartid
-     call print_reg
+    li a0, "|mhtid:" 
+    call print7
+    csrr a0, mhartid
+    call print_reg
 
+    li a0, "|mstas:" 
+    call print7
+    csrr a0, mstatus
+    call print_reg
 
 
      li a0, "|" 
