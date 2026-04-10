bc4b503e5dadab8af16bc8de0676b94ef71edef9
youhao <youhaohao@haohaohao.com>
2026-04-10 14:10:13 +0800

    update

--- cpu_on_board/qemu_opensbi.sh
+++ cpu_on_board/qemu_opensbi.sh
@@ -1,4 +1,13 @@
-qemu-system-riscv64 -M virt -m 64M -nographic -bios fw_jump.bin -dtb my_board.dtb
+#qemu-system-riscv64 -M virt -m 64M -nographic -bios fw_jump_soc.bin -dtb my_board_soc.dtb
+qemu-system-riscv64  \
+    -M virt \
+    -m 64M \
+    -cpu rv64,f=off,d=off,c=off,h=off,g=off,zfa=off,zfh=off,zba=off,zbb=off \
+    -nographic \
+    -bios fw_jump_qemu.bin \
+    -dtb my_board_qemu.dtb \
+
+
 
 # 1. qemu-ima test jump
 # 2. test payload
