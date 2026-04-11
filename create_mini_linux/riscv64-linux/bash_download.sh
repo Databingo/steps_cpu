6b781528bbcd69e4193ed8d2696cdda4d7355154
root <root@localhost.localdomain>
2026-04-11 22:50:30 +0800

    update

--- create_mini_linux/riscv64-linux/bash_download.sh
+++ create_mini_linux/riscv64-linux/bash_download.sh
@@ -7,3 +7,6 @@ cd linux && make ARCH=riscv CROSS_COMPILE=/usr/local/projects/steps_cpu/create_m
 cp linux/.config config 
 
 cd linux && make ARCH=riscv CROSS_COMPILE=/usr/local/projects/steps_cpu/create_mini_linux/buildroot-2026.02/output/host/bin/riscv64-buildroot-linux-musl- -j$(nproc)
+
+
+cp linux/arch/riscv/boot/Image .
