4090efe5a7b5705196329b334f15448e5c906ef0
youhao <youhaohao@haohaohao.com>
2026-04-11 22:50:40 +0800

    update

--- cpu_on_board/qemuopenlinuxbusy.sh
+++ cpu_on_board/qemuopenlinuxbusy.sh
@@ -7,12 +7,17 @@
 #    -bios opensbi/build/platform/generic/firmware/fw_payload.bin \
 #   #-bios fw_jump_qemu.bin \
 #
+#qemu-system-riscv64 -nographic \
+#    -machine virt \
+#    -cpu rv64,f=off,d=off,c=off,h=off,g=off,zfa=off,zfh=off,zba=off,zbb=off \
+#    -bios opensbi/build/platform/generic/firmware/fw_payload.bin \
+##   -append "console=hvc0 earlycon=sbi"
+
 qemu-system-riscv64 -nographic \
     -machine virt \
     -cpu rv64,f=off,d=off,c=off,h=off,g=off,zfa=off,zfh=off,zba=off,zbb=off \
-    -bios opensbi/build/platform/generic/firmware/fw_payload.bin \
-#   -append "console=hvc0 earlycon=sbi"
-
+    -kernel /usr/local/projects/steps_cpu/create_mini_linux/riscv64-linux/Image \
+    -append "console=hvc0 earlycon=sbi"
 
 # 1. qemu-ima test jump
 # 2. test payload
