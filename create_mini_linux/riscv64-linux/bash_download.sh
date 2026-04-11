f809cc77bad3812c45f9a60ea52036c37f2acd7d
root <root@localhost.localdomain>
2026-04-11 20:53:33 +0800

    update

--- create_mini_linux/riscv64-linux/bash_download.sh
+++ create_mini_linux/riscv64-linux/bash_download.sh
@@ -1,5 +1,6 @@
 #git clone --depth 1 --branch v6.12 https://github.com/torvalds/linux.git 
 # unFPU unCompressedIR PageTablLevel3 bootcommand
-#cp config linux/.config
-#cd linux && make ARCH=riscv CROSS_COMPILE=/usr/local/projects/steps_cpu/create_mini_linux/buildroot-2026.02/output/host/bin/riscv64-buildroot-linux-musl- menuconfig
-cd linux && make ARCH=riscv CROSS_COMPILE=/usr/local/projects/steps_cpu/create_mini_linux/buildroot-2026.02/output/host/bin/riscv64-buildroot-linux-musl- -j$(nproc)
+cd linux && make clean && cd -
+cp config linux/.config
+cd linux && make ARCH=riscv CROSS_COMPILE=/usr/local/projects/steps_cpu/create_mini_linux/buildroot-2026.02/output/host/bin/riscv64-buildroot-linux-musl- menuconfig
+#cd linux && make ARCH=riscv CROSS_COMPILE=/usr/local/projects/steps_cpu/create_mini_linux/buildroot-2026.02/output/host/bin/riscv64-buildroot-linux-musl- -j$(nproc)
