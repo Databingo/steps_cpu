521b060dccb3498328fd697c666b034e4aa9e310
root <root@localhost.localdomain>
2026-04-11 08:49:57 +0800

    update

--- create_mini_linux/bbusybox.sh
+++ create_mini_linux/bbusybox.sh
@@ -1,20 +1,43 @@
-# Build Busybox
+## Build Busybox
+##git clone --depth 1 https://git.busybox.net/busybox
+##cd busybox && CROSS_COMPILE=/usr/local/projects/bin/riscv64-lp64d--glibc--bleeding-edge-2025.08-1/bin/riscv64-buildroot-linux-gnu- make clean  && cd -
+##cd busybox && CROSS_COMPILE=/usr/local/projects/bin/riscv64-lp64d--glibc--bleeding-edge-2025.08-1/bin/riscv64-buildroot-linux-gnu- make defconfig && cd -
+##cd busybox && CROSS_COMPILE=/usr/local/projects/bin/riscv64-lp64d--glibc--bleeding-edge-2025.08-1/bin/riscv64-buildroot-linux-gnu- make menuconfig && cd -
+##cd busybox && CROSS_COMPILE=/usr/local/projects/bin/riscv64-lp64d--glibc--bleeding-edge-2025.08-1/bin/riscv64-buildroot-linux-gnu- make -j $(nproc) && cd -
+#
+#
+## ISA=imafd
+#set -e
+#
+#export ARCH="riscv"
+#export CROSS_COMPILE="/usr/local/projects/bin/riscv64-lp64d--glibc--bleeding-edge-2025.08-1/bin/riscv64-buildroot-linux-gnu-"
+#export CFLAGS="-march=rv64imafd -mabi=lp64d"
+#
+##Build kernel
+##cd riscv64-linux/linux && make clean && cd -
+##cd riscv64-linux/linux && make defconfig && cd -
+##cd riscv64-linux/linux && make menuconfig && cd -
+#cd busybox && make -j $(nproc) && cd -
+#
+
+#
 #git clone --depth 1 https://git.busybox.net/busybox
-#cd busybox && CROSS_COMPILE=/usr/local/projects/bin/riscv64-lp64d--glibc--bleeding-edge-2025.08-1/bin/riscv64-buildroot-linux-gnu- make clean  && cd -
-#cd busybox && CROSS_COMPILE=/usr/local/projects/bin/riscv64-lp64d--glibc--bleeding-edge-2025.08-1/bin/riscv64-buildroot-linux-gnu- make defconfig && cd -
-#cd busybox && CROSS_COMPILE=/usr/local/projects/bin/riscv64-lp64d--glibc--bleeding-edge-2025.08-1/bin/riscv64-buildroot-linux-gnu- make menuconfig && cd -
-#cd busybox && CROSS_COMPILE=/usr/local/projects/bin/riscv64-lp64d--glibc--bleeding-edge-2025.08-1/bin/riscv64-buildroot-linux-gnu- make -j $(nproc) && cd -
+# Settings Build static binary (no shared libs)
+# Settings Additional CFLAGS: -march=rv64ima_zicsr_zifencei -mabi=lp64
+#
+#cd busybox && make distclean && cd -
+#cd busybox && make defconfig && cd -
+#cd busybox && make menuconfig && cd -
 
-
-# ISA=imafd
-set -e
-
-export ARCH="riscv"
-export CROSS_COMPILE="/usr/local/projects/bin/riscv64-lp64d--glibc--bleeding-edge-2025.08-1/bin/riscv64-buildroot-linux-gnu-"
-export CFLAGS="-march=rv64imafd -mabi=lp64d"
-
-#Build kernel
-#cd riscv64-linux/linux && make clean && cd -
-#cd riscv64-linux/linux && make defconfig && cd -
-#cd riscv64-linux/linux && make menuconfig && cd -
-cd busybox && make -j $(nproc) && cd -
+#cd busybox && make -j $(nproc) && cd -
+#cd busybox &&  make CROSS_COMPILE=riscv64-linux-gnu- \
+#	            EXTRA_CCASFLAGS="-march=rv64ima_zicsr_zifencei -mabi=lp64"\
+#                    EXTRA_CFLAGS="-mno-relax -march=rv64ima_zicsr_zifencei -mabi=lp64"\
+#		    -j $(nproc)
+#
+# make CROSS_COMPILE=riscv64-linux-gnu- install
+#
+#
+#
+#
+#
--- create_mini_linux/prepare.sh
+++ create_mini_linux/prepare.sh
@@ -10,3 +10,27 @@
 # Settings Additional CFLAGS: -march=rv64ima_zicsr_zifencei -mabi=lp64
 # make CROSS_COMPILE=riscv64-linux-gnu- -j $(nproc)
 # make CROSS_COMPILE=riscv64-linux-gnu- install
+#
+#
+#
+#wget https://buildroot.org/downloads/buildroot-2026.02.tar.gz
+#tar -zxvf buildroot-2026.02.tar.gz
+cd buildroot-2026.02
+
+cat <<EOF > ima_defconfig
+BR2_riscv=y
+BR2_RISCV_64=y
+BR2_riscv_custom=y
+BR2_RISCV_ISA_CUSTOM_RVM=y
+BR2_RISCV_ISA_CUSTOM_RVA=y
+BR2_RISCV_ISA_CUSTOM_EXTENSIONS="-march=rv64ima_zicsr_zifencei_zalrsc_zaamo"
+BR2_RISCV_ABI_LP64=y
+BR2_TOOLCHAIN_BUILDROOT_MUSL=y
+BR2_TARGET_ROOTFS_CPIO=y
+EOF
+
+
+
+LD_LIBRARY_PATH= make defconfig BR2_DEFCONFIG=ima_defconfig
+LD_LIBRARY_PATH= make clean
+LD_LIBRARY_PATH= FORCE_UNSAFE_CONFIGURE=1 make -j$(nproc)
