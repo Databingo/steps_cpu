1ab03b6dfbebbf64ad7860f3ff8bf575da4c5658
root <root@localhost.localdomain>
2026-04-11 11:11:54 +0800

    update

--- create_mini_linux/prepare.sh
+++ create_mini_linux/prepare.sh
@@ -31,10 +31,10 @@ BR2_STATIC_LIBS=y
 EOF
 
 
-
-LD_LIBRARY_PATH= make clean
-LD_LIBRARY_PATH= make defconfig BR2_DEFCONFIG=ima_defconfig
-LD_LIBRARY_PATH= FORCE_UNSAFE_CONFIGURE=1 make -j$(nproc)
+#
+#LD_LIBRARY_PATH= make clean
+#LD_LIBRARY_PATH= make defconfig BR2_DEFCONFIG=ima_defconfig
+#LD_LIBRARY_PATH= FORCE_UNSAFE_CONFIGURE=1 make -j$(nproc)
 
 # build process
 # binutils (rv assembler/linker)
