c27996e051ea1fcde1e46b2454badfc176daac2c
youhao <youhaohao@haohaohao.com>
2026-04-12 09:54:28 +0800

    update

--- cpu_on_board/qemuopenlinuxbusy.sh
+++ cpu_on_board/qemuopenlinuxbusy.sh
@@ -33,13 +33,22 @@
 #    -append "console=ttyS0"
 #
 
+# Work turn to page 3 with same Image of linux
 qemu-system-riscv64 -nographic \
     -machine virt \
     -cpu rv64,f=off,d=off,c=off,h=off,g=off,zfa=off,zfh=off,zba=off,zbb=off \
     -bios fw_jump_ima.bin \
-    -kernel /usr/local/projects/steps_cpu/create_mini_linux/riscv64-linux/Image_sbiuart_page3 \
+    -kernel /usr/local/projects/steps_cpu/create_mini_linux/riscv64-linux/Image_default_uart_page5 \
     -initrd /usr/local/projects/steps_cpu/create_mini_linux/riscv64-linux/rootfs.cpio \
-    -append "console=ttyS0"
+    -append "no4lvl console=ttyS0"
+
+#qemu-system-riscv64 -nographic \
+#    -machine virt \
+#    -cpu rv64,f=off,d=off,c=off,h=off,g=off,zfa=off,zfh=off,zba=off,zbb=off \
+#    -bios fw_jump_ima.bin \
+#    -kernel /usr/local/projects/steps_cpu/create_mini_linux/riscv64-linux/Image_sbiuart_page3 \
+#    -initrd /usr/local/projects/steps_cpu/create_mini_linux/riscv64-linux/rootfs.cpio \
+#    -append "no4lvl console=hvc0 earlycon=sbi"
 
 # 1. qemu-ima test jump
 # 2. test payload
