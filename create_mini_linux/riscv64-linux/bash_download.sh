2d80edd75c902889042bbe00f93fcb79bda83b27
root <root@localhost.localdomain>
2026-04-12 10:27:13 +0800

    update

--- create_mini_linux/riscv64-linux/bash_download.sh
+++ create_mini_linux/riscv64-linux/bash_download.sh
@@ -2,11 +2,11 @@
 # unFPU unCompressedIR PageTablLevel3 bootcommand
 #cd linux && make clean && cd -
  
-#cp config linux/.config
-#cd linux && make ARCH=riscv CROSS_COMPILE=/usr/local/projects/steps_cpu/create_mini_linux/buildroot-2026.02/output/host/bin/riscv64-buildroot-linux-musl- menuconfig && cd -
-#cp linux/.config config 
+cp config linux/.config
+cd linux && make ARCH=riscv CROSS_COMPILE=/usr/local/projects/steps_cpu/create_mini_linux/buildroot-2026.02/output/host/bin/riscv64-buildroot-linux-musl- menuconfig && cd -
+cp linux/.config config 
 #
-#cd linux && make ARCH=riscv CROSS_COMPILE=/usr/local/projects/steps_cpu/create_mini_linux/buildroot-2026.02/output/host/bin/riscv64-buildroot-linux-musl- -j$(nproc) && cd -
+cd linux && make ARCH=riscv CROSS_COMPILE=/usr/local/projects/steps_cpu/create_mini_linux/buildroot-2026.02/output/host/bin/riscv64-buildroot-linux-musl- -j$(nproc) && cd -
 
 cp linux/arch/riscv/boot/Image .
 cp ../buildroot-2026.02/output/images/rootfs.cpio .
