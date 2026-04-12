eb9e8c621f959fa0eb49aa035173e346a4626e89
root <root@localhost.localdomain>
2026-04-12 17:05:37 +0800

    update

--- create_mini_linux/riscv64-linux/bash_download.sh
+++ create_mini_linux/riscv64-linux/bash_download.sh
@@ -9,4 +9,5 @@ cp linux/.config config
 cd linux && make ARCH=riscv CROSS_COMPILE=/usr/local/projects/steps_cpu/create_mini_linux/buildroot-2026.02/output/host/bin/riscv64-buildroot-linux-musl- -j$(nproc) && cd -
 
 cp linux/arch/riscv/boot/Image .
+cp linux/arch/riscv/boot/Image.xz .
 cp ../buildroot-2026.02/output/images/rootfs.cpio .
