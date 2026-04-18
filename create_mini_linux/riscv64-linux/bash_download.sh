9c721fee8c5a81cef3b9282eb836a348f2fd631e
youhao <youhaohao@haohaohao.com>
2026-04-18 22:11:10 +0800

    update

--- create_mini_linux/riscv64-linux/bash_download.sh
+++ create_mini_linux/riscv64-linux/bash_download.sh
@@ -3,8 +3,8 @@
 #cd linux && make clean && cd -
  
 cp config linux/.config
-#cd linux && make ARCH=riscv CROSS_COMPILE=/usr/local/projects/steps_cpu/create_mini_linux/buildroot-2026.02/output/host/bin/riscv64-buildroot-linux-musl- menuconfig && cd -
-cd linux && make ARCH=riscv CROSS_COMPILE=/usr/local/projects/steps_cpu/create_mini_linux/buildroot-2026.02/output/host/bin/riscv64-buildroot-linux-musl- oldconfig && cd -
+cd linux && make ARCH=riscv CROSS_COMPILE=/usr/local/projects/steps_cpu/create_mini_linux/buildroot-2026.02/output/host/bin/riscv64-buildroot-linux-musl- menuconfig && cd -
+#cd linux && make ARCH=riscv CROSS_COMPILE=/usr/local/projects/steps_cpu/create_mini_linux/buildroot-2026.02/output/host/bin/riscv64-buildroot-linux-musl- oldconfig && cd -
 cp linux/.config config 
 #
 cd linux && make ARCH=riscv CROSS_COMPILE=/usr/local/projects/steps_cpu/create_mini_linux/buildroot-2026.02/output/host/bin/riscv64-buildroot-linux-musl- -j$(nproc) && cd -
