45925058921d6a75a8713f39b3889318ab78f9f9
youhao <youhaohao@haohaohao.com>
2026-04-21 15:54:09 +0800

    update

--- cpu_on_board/z.sh
+++ cpu_on_board/z.sh
@@ -6,11 +6,14 @@ cat sdram_controller.v >> z2.txt
 cat header.vh >> z2.txt 
 echo "---------- mmu-itlb-dtlb, icache isr.s code---------" >> z2.txt
 cat isr.s >> z2.txt 
-#echo "---------- mini_kernel test code ---------" >> z2.txt
-#cat mini_kernel.s >> z2.txt
+echo "---------- mini_kernel test code ---------" >> z2.txt
+cat mini_kernel.s >> z2.txt
 echo "---------- device tree ---------" >> z2.txt
 cat opensbi/b.dts >> z2.txt
 echo "---------- opensbi config ---------" >> z2.txt
 cat opensbi/bash_opensbi.sh >> z2.txt
 echo "---------- bootloader code ---------" >> z2.txt
 cat bootloader.s >> z2.txt
+# gcc-musl ima
+# linux ima small
+# buxybox ima small
