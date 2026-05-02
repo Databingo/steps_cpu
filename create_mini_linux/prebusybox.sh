f07de0aace3bc7a7c1bd168a67282a3dafc4bfc0
root <root@localhost.localdomain>
2026-05-02 10:30:06 +0800

    update

--- create_mini_linux/prebusybox.sh
+++ create_mini_linux/prebusybox.sh
@@ -1,5 +1,8 @@
 cd buildroot-2026.02
 LD_LIBRARY_PATH= make busybox-menuconfig 
+LD_LIBRARY_PATH= make target-finalize
+LD_LIBRARY_PATH= FORCE_UNSAFE_CONFIGURE=1 make 
+
 LD_LIBRARY_PATH= FORCE_UNSAFE_CONFIGURE=1 make busybox-rebuild
 LD_LIBRARY_PATH= FORCE_UNSAFE_CONFIGURE=1 make 
 #LD_LIBRARY_PATH= FORCE_UNSAFE_CONFIGURE=1 make busybox-update-config
