d4a25144842d26674a8441c1cbbf2f5b5b40d098
youhao <youhaohao@haohaohao.com>
2026-05-06 18:55:39 +0800

    update

--- create_mini_linux/riscv64-linux/hello.c
+++ create_mini_linux/riscv64-linux/hello.c
@@ -53,10 +53,21 @@
 //}
 
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/mount.h>
 
 int main() {
+    mkdir("/dev", 0755);
+    mount("devtmpfs", "/dev", "devtmpfs", 0, NULL);
+    int fd=open("/dev/hvc0", O_RDWR);
+    dup2(fd, 0);
+    dup2(fd, 1);
+    dup2(fd, 2);
+
+
+    write(1, "OK\r\n", 4);
+
     char c;
-    write(1, "OK\n", 3);
     while(1) {
 	if (read(0, &c, 1) > 0) { write(1, "U", 1); }
 	}
