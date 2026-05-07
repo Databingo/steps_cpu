975f0fe24a9b54f81a0a4fd54c13649c4422e42b
youhao <youhaohao@haohaohao.com>
2026-05-07 09:19:56 +0800

    update

--- create_mini_linux/riscv64-linux/hello.s
+++ create_mini_linux/riscv64-linux/hello.s
@@ -1,6 +1,13 @@
-.section .text
 .global _start
 
+.section .data
+msg: 
+   .string "ASM INIT STARTING\n"
+buf: 
+   .word 0
+
+
+.section .text
 _start:
     # write(1, msg, 18)
     li a0, 1            # fd = 1
@@ -23,9 +30,11 @@ loop:
     li a2, 1            # len
     li a7, 64           # syscall write
     ecall
+    
+    # Sign
+    addi x0, x0, 0x11  # 0x01100013
+    addi x0, x0, 0x22  # 0x02200013
+    addi x0, x0, 0x33  # 0x03300013
 
     j loop
 
-.section .data
-msg: .ascii "ASM INIT STARTING\n"
-buf: .byte 0
--- create_mini_linux/riscv64-linux/hello.sh
+++ create_mini_linux/riscv64-linux/hello.sh
@@ -1,3 +1,4 @@
-/usr/local/projects/steps_cpu/create_mini_linux/buildroot-2026.02/output/host/bin/riscv64-buildroot-linux-musl-gcc -static hello.c -o hello
+#/usr/local/projects/steps_cpu/create_mini_linux/buildroot-2026.02/output/host/bin/riscv64-buildroot-linux-musl-gcc -static hello.c -o hello
+/usr/local/projects/steps_cpu/create_mini_linux/buildroot-2026.02/output/host/bin/riscv64-buildroot-linux-musl-gcc -static hello.s -o hello
 chmod 777 hello
 cp hello unpacked_rootfs/
