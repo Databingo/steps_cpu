72bf6c68f20fcf821ae7a0cab2f30ca64265718f
youhao <youhaohao@haohaohao.com>
2026-05-08 08:20:07 +0800

    update

--- cpu_on_board/riscv64.v
+++ cpu_on_board/riscv64.v
@@ -756,7 +756,8 @@ wire is_store = (op == 7'b0100011) || (op == 7'b0101111 && w_func5 != 5'b00010);
 		//re[8] <= (op == 7'b0000011) ? 13 : 14;// save x2 trap type load/store_atom
 		re[8] <= 18; // save 18 for debug
                 Csrs[mimpid] <= pc_4; // pc
-                Csrs[marchid] <= Csrs[mip];  // mip
+                //Csrs[marchid] <= Csrs[mip];  // mip
+                Csrs[marchid] <= re[10]; // a0
                 Csrs[mvendorid] <= ir;
 
             // Back from STrap
